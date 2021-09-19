1f292f409638858c1cb5b1bb535827f73860f175
Dennis Horte <dennis@pinterest.com>
2021-09-18 18:32:59 -0700

    Visually distinguish development from prod

--- app/src/App.vue
+++ app/src/App.vue
@@ -6,7 +6,13 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+
+  mounted() {
+    if(process.env.NODE_ENV === 'development') {
+      document.querySelector('body').style.backgroundImage = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='50px' width='120px'><text x='0' y='15' fill='rgb(240, 240, 240)' font-size='20'>development</text></svg>")`
+    }
+  }
 }
 </script>
 
