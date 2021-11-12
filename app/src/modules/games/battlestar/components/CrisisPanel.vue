c762560161722bc24ea8e38a099e46cfe653b433
Dennis Horte <dennis@pinterest.com>
2021-11-12 07:48:29 -0800

    Clean up the crisis panel

--- app/src/modules/games/battlestar/components/CrisisPanel.vue
+++ app/src/modules/games/battlestar/components/CrisisPanel.vue
@@ -2,82 +2,36 @@
   <div class="main-crisis">
     <div class="description">
 
-      <b-button
-        v-if="!activeCrisis"
-        @click="drawCrisis"
-        variant="primary"
-        block>
-        draw crisis card
-      </b-button>
-
-      <b-button-group v-else class="w-100 mb-2" style="background-color: white;">
-        <b-button
-          @click="setStep('discuss')"
-          :variant="crisisStep === 'discuss' ? 'primary' : 'outline-primary'">
-          discuss
-        </b-button>
-
-        <b-button
-          @click="setStep('decide')"
-          :variant="crisisStep === 'decide' ? 'primary' : 'outline-primary'">
-          decide
-        </b-button>
-
-        <b-button
-          @click="setStep('add cards')"
-          :variant="crisisStep === 'add cards' ? 'primary' : 'outline-primary'">
-          add cards
-        </b-button>
-
-        <b-button
-          @click="setStep('resolve')"
-          :variant="crisisStep === 'resolve' ? 'primary' : 'outline-primary'">
-          resolve
-        </b-button>
-      </b-button-group>
-
-      <b-button-group v-if="isSkillCheck && crisisStep === 'discuss'" class="w-100 mb-2">
-        <b-button @click="help('none')" variant="danger">none</b-button>
-        <b-button @click="help('a little')" variant="warning">a little</b-button>
-        <b-button @click="help('a lot')" variant="primary">a lot</b-button>
-      </b-button-group>
-
       <b-row>
         <b-col>
           <CrisisCard v-if="card.name" :card="card" />
         </b-col>
 
         <b-col v-if="isSkillCheck">
+          <span class="faint font-italic ml-2">
+            in order of adding cards
+          </span>
+
           <div
             v-for="player in playersOrdered"
             :key="player.name"
           >
             <div class="heading">{{ player.name }}</div>
             <div class="indented">
-              <span v-if="!!player.crisisHelp">
-                {{ player.crisisHelp }}
+              <span v-if="!!discussion[player.name].support">
+                {{ discussion[player.name].support }}
               </span>
               <span v-else class="faint">
                 waiting
               </span>
 
-              <span v-if="player.crisisCount >= 0">
-                {{ player.crisisCount }}
+              <span v-if="cardsAdded[player.name].submitted">
+                {{ cardsAdded[player.name].numAdded }}
               </span>
               <span v-else class="faint">
                 waiting
               </span>
             </div>
-          </div>
-
-          <div v-if="crisisStep === 'add cards'">
-            <b-button variant="warning" @click="addDestinyCards" :disabled="crisisDestinyAdded">
-              Add Destiny Cards
-            </b-button>
-
-            <b-button variant="success" @click="doneAdding" :disabled="crisisDone">
-              Done Adding My Cards
-            </b-button>
           </div>
 
         </b-col>
@@ -106,20 +60,12 @@ export default {
       return this.$game.getCrisis() || {}
     },
 
-    activeCrisis() {
-      return this.$game.getCardActiveCrisis()
+    cardsAdded() {
+      return this.$game.getSkillCheck().addCards
     },
 
-    crisisDestinyAdded() {
-      return false
-    },
-
-    crisisDone() {
-      return false
-    },
-
-    crisisStep() {
-      return ''
+    discussion() {
+      return this.$game.getSkillCheck().discussion
     },
 
     isSkillCheck() {
@@ -137,29 +83,6 @@ export default {
       return players
     },
   },
-
-  methods: {
-    addDestinyCards() {
-      console.log('add destiny cards')
-    },
-
-    drawCrisis() {
-      console.log('draw crisis card')
-    },
-
-    doneAdding() {
-      console.log('done adding cards')
-    },
-
-    help(amount) {
-      console.log('help: ', amount)
-    },
-
-    setStep(name) {
-      console.log('set crisis step', name)
-    },
-  },
-
 }
 </script>
 
@@ -171,7 +94,7 @@ export default {
 }
 
 .faint {
-  color: gray;
+  color: #ccc;
 }
 
 .indented {
