51ad208a4e27dcf32dcee020f9c7d795813c29fb
Dennis Horte <dennis@pinterest.com>
2022-01-07 12:22:32 -0800

    Update to the latest share rules for figures

--- common/innovation/transitions/rawDraw.js
+++ common/innovation/transitions/rawDraw.js
@@ -4,11 +4,15 @@ module.exports = function(context) {
 
   // Determine which expansion to draw from.
   let exp = 'base'
-  if (game.getExpansionList().includes('figs')) {
-    exp = 'figs'
-  }
-  else if (game.getExpansionList().includes('echo')) {
+  if (game.getExpansionList().includes('echo')) {
     throw new Error('Drawing cards not supported in Echoes of the Past yet.')
+  }
+  if (
+    game.getExpansionList().includes('figs')
+    && isShareBonus
+    && exp === 'base'
+  ) {
+    exp = 'figs'
   }
 
   // If age is not specified, draw based on player's current highest top card.
