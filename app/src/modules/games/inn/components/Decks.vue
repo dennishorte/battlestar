f8f3b05433db664de8fbfa89a4b6ce1ebe2e9de9
Dennis Horte <dennis@pinterest.com>
2022-03-08 09:15:37 -0800

    Improved decks component

--- app/src/modules/games/inn/components/Decks.vue
+++ app/src/modules/games/inn/components/Decks.vue
@@ -1,24 +1,50 @@
 <template>
   <div class="decks">
-    <div class="decks-row">
-      <div class="decks-box"></div>
-      <div
-        v-for="exp in expansions"
-        :key="exp"
-        class="decks-box"
-        :class="`text-${exp}`">
-        {{ exp[0].toUpperCase() }}
-      </div>
-    </div>
 
-    <div v-for="age in [10,9,8,7,6,5,4,3,2,1]" :key="age" class="decks-row">
-      <div class="decks-box">{{ age }}</div>
-      <div
-        v-for="exp in expansions"
-        :key="exp"
-        class="decks-box"
-        :class="`text-${exp}`">
-        {{ game.getZoneByDeck(exp, age).cards().length }}
+    <b-row v-if="doubleColumn">
+      <b-col lg="auto">
+        <div v-for="age in [10,9,8,7,6]" :key="age" class="decks-row">
+          <div class="decks-box">
+            <div class="age-box">{{ age }}</div>
+          </div>
+          <div
+            v-for="exp in expansions"
+            :key="exp"
+            class="decks-box"
+            :class="`text-${exp}`">
+            {{ game.getZoneByDeck(exp, age).cards().length }}
+          </div>
+        </div>
+      </b-col>
+
+      <b-col lg="auto">
+        <div v-for="age in [5,4,3,2,1]" :key="age" class="decks-row">
+          <div class="decks-box">
+            <div class="age-box">{{ age }}</div>
+          </div>
+          <div
+            v-for="exp in expansions"
+            :key="exp"
+            class="decks-box"
+            :class="`text-${exp}`">
+            {{ game.getZoneByDeck(exp, age).cards().length }}
+          </div>
+        </div>
+      </b-col>
+    </b-row>
+
+    <div v-else>
+      <div v-for="age in [10,9,8,7,6,5,4,3,2,1]" :key="age" class="decks-row">
+        <div class="decks-box">
+          <div class="age-box">{{ age }}</div>
+        </div>
+        <div
+          v-for="exp in expansions"
+          :key="exp"
+          class="decks-box"
+          :class="`text-${exp}`">
+          {{ game.getZoneByDeck(exp, age).cards().length }}
+        </div>
       </div>
     </div>
   </div>
@@ -31,6 +57,10 @@ export default {
   inject: ['game'],
 
   computed: {
+    doubleColumn() {
+      return this.expansions.length < 3
+    },
+
     expansions() {
       return ['arti', 'city', 'figs', 'echo', 'base']
         .filter(exp => this.game.getExpansionList().includes(exp))
@@ -50,5 +80,14 @@ export default {
   height: 1.2rem;
   width: 1.5rem;
   text-align: right;
+  margin-bottom: 1px;
+  padding-right: 4px;
+}
+
+.age-box {
+  padding: 0;
+  color: white;
+  background-color: #bba37a;
+  text-align: center;
 }
 </style>
