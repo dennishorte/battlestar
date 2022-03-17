1d8253a7add727cdbc105eee797b89b33aaced9e
Dennis Horte <dennis@pinterest.com>
2022-03-17 11:23:19 -0700

    Baghdad Battery

--- common/innovation/res/arti/BaghdadBattery.js
+++ common/innovation/res/arti/BaghdadBattery.js
@@ -15,7 +15,22 @@ function Card() {
     `Meld two cards from your hand. If you melded two of the same color and they are of different type, draw and score five {2}s.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const cards = game.aChooseAndMeld(player, game.getCardsByZone(player, 'hand'), { count: 2 })
+
+      if (
+        cards
+        && cards.length === 2
+        && cards[0].color === cards[1].color
+        && cards[0].expansion !== cards[1].expansion
+      ) {
+        for (let i = 0; i < 5; i++) {
+          game.aDrawAndScore(player, game.getEffectAge(this, 2))
+        }
+      }
+    },
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
