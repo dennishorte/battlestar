911fc71a733802653b9feccc2da919525bfaee69
Dennis Horte <dennis@pinterest.com>
2022-03-18 13:45:27 -0700

    Buttonwood Agreement

--- common/innovation/res/arti/ButtonwoodAgreement.js
+++ common/innovation/res/arti/ButtonwoodAgreement.js
@@ -12,10 +12,27 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `Choose three colors. Draw and reveal a {8}. If the rawn card is one of the chosen colors, score it and splay up that color. Otherwise, return all cards of the drawn card's color from your score pile, and unsplay that color.`
+    `Choose three colors. Draw and reveal a {8}. If the drawn card is one of the chosen colors, score it and splay up that color. Otherwise, return all cards of the drawn card's color from your score pile, and unsplay that color.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const colors = game.aChoose(player, game.utilColors(), { count: 3 })
+      const card = game.aDrawAndReveal(player, game.getEffectAge(this, 8))
+
+      if (colors.includes(card.color)) {
+        game.aScore(player, card)
+        game.aSplay(player, card.color, 'up')
+      }
+      else {
+        const toReturn = game
+          .getCardsByZone(player, 'score')
+          .filter(other => other.color === card.color)
+        game.aReturnMany(player, toReturn)
+        game.aUnsplay(player, game.getZoneByPlayer(player, card.color))
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
