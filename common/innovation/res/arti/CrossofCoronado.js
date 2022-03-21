8a4c2c4f01ab1dfbce6c80714eb35b14909629ba
Dennis Horte <dennis@pinterest.com>
2022-03-21 09:43:36 -0700

    Cross of Coronado

--- common/innovation/res/arti/CrossofCoronado.js
+++ common/innovation/res/arti/CrossofCoronado.js
@@ -1,4 +1,6 @@
 const CardBase = require(`../CardBase.js`)
+const { GameOverEvent } = require('../../game.js')
+const util = require('../../../lib/util.js')
 
 function Card() {
   this.id = `Cross of Coronado`  // Card names are unique in Innovation
@@ -15,7 +17,22 @@ function Card() {
     `Reveal your hand. If you have exactly five cards and five colors in your hand, you win.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const cards = game.getCardsByZone(player, 'hand')
+      for (const card of cards) {
+        game.mReveal(player, card)
+      }
+
+      const colors = util.array.distinct(cards.map(card => card.color))
+      if (cards.length === 5 && colors.length === 5) {
+        throw new GameOverEvent({
+          player,
+          reason: this.name
+        })
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
