82f96e042562a34df7fdf5900cfbd46d1cec87c8
Dennis Horte <dennis@pinterest.com>
2022-03-20 22:05:05 -0700

    Chronicle of Zuo

--- common/innovation/res/arti/ChronicleofZuo.js
+++ common/innovation/res/arti/ChronicleofZuo.js
@@ -15,7 +15,35 @@ function Card() {
     `If you have the least {k}, draw a {2}. If you have the least {c}, draw a {3}. If you have the least {s}, draw a {4}.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const biscuits = game.getBiscuits()
+      const castles = game
+        .getPlayerOpponents(player)
+        .map(opp => biscuits[opp.name].k)
+        .sort((l, r) => l - r)
+      const coins = game
+        .getPlayerOpponents(player)
+        .map(opp => biscuits[opp.name].c)
+        .sort((l, r) => l - r)
+      const lights = game
+        .getPlayerOpponents(player)
+        .map(opp => biscuits[opp.name].s)
+        .sort((l, r) => l - r)
+
+      if (biscuits[player.name].k < castles[0]) {
+        game.aDraw(player, { age: game.getEffectAge(this, 2) })
+      }
+
+      if (biscuits[player.name].c < coins[0]) {
+        game.aDraw(player, { age: game.getEffectAge(this, 3) })
+      }
+
+      if (biscuits[player.name].s < lights[0]) {
+        game.aDraw(player, { age: game.getEffectAge(this, 4) })
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
