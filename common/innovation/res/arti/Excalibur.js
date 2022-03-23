8097dd4ab93362a4b9b01c0ca442963a7c9ba41d
Dennis Horte <dennis@pinterest.com>
2022-03-23 16:43:39 -0700

    Excalibur

--- common/innovation/res/arti/Excalibur.js
+++ common/innovation/res/arti/Excalibur.js
@@ -15,7 +15,35 @@ function Card() {
     `I compel your to transfer a top card of higher value than my top card of the same color from your board to my board!`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player, { leader }) => {
+      const choices = game
+        .utilColors()
+        .map(color => {
+          const playerTop = game.getTopCard(player, color)
+          const leaderTop = game.getTopCard(leader, color)
+
+          if (!playerTop) {
+            return undefined
+          }
+          else if (!leaderTop) {
+            return playerTop
+          }
+          else if (playerTop.getAge() > leaderTop.getAge()) {
+            return playerTop
+          }
+          else {
+            return undefined
+          }
+        })
+        .filter(card => !!card)
+
+      const card = game.aChooseCard(player, choices)
+      if (card) {
+        game.aTransfer(player, card, game.getZoneByPlayer(leader, card.color))
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
