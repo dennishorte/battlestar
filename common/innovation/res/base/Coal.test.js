a3cf9daa1e7dfd1b4a6119328fc5f4e5831b0084
Dennis Horte <dennis@pinterest.com>
2022-03-23 10:03:01 -0700

    bug fix: Coal: could not decline to score a top card

--- common/innovation/res/base/Coal.test.js
+++ common/innovation/res/base/Coal.test.js
@@ -20,4 +20,20 @@ describe('Coal', () => {
     expect(t.zone(game, 'red').splay).toStrictEqual('right')
   })
 
+  test('dogma: choose not to score', () => {
+    const game = t.fixtureTopCard('Coal')
+    game.testSetBreakpoint('before-first-player', (game) => {
+      t.setColor(game, 'dennis', 'blue', ['Alchemy', 'Calendar', 'Tools'])
+      t.setDeckTop(game, 'base', 5, ['The Pirate Code'])
+    })
+    const result1 = game.run()
+    const result2 = t.choose(game, result1, 'Dogma.Coal')
+    const result3 = t.choose(game, result2, 'red')
+    const result4 = t.choose(game, result3)
+
+    expect(t.cards(game, 'blue')).toStrictEqual(['Alchemy', 'Calendar', 'Tools'])
+    expect(t.cards(game, 'red')).toStrictEqual(['Coal', 'The Pirate Code'])
+    expect(t.zone(game, 'red').splay).toStrictEqual('right')
+  })
+
 })
