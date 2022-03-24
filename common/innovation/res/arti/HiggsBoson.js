35b984608dec828467658c1a5db254d1c333afaa
Dennis Horte <dennis@pinterest.com>
2022-03-24 14:42:50 -0700

    Higgs Boson

--- common/innovation/res/arti/HiggsBoson.js
+++ common/innovation/res/arti/HiggsBoson.js
@@ -15,7 +15,40 @@ function Card() {
     `Transfer all cards on your board to your score pile.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const autoReturn = game.aYesNo(player, 'Auto return cards?')
+      const scoreZone = game.getZoneByPlayer(player, 'score')
+
+      if (autoReturn) {
+        const cards = game
+          .utilColors()
+          .flatMap(color => game.getCardsByZone(player, color))
+
+        game.aTransferMany(player, cards, scoreZone, { ordered: true })
+      }
+      else {
+        while (true) {
+          const choices = game.getTopCards(player)
+          if (choices.length === 0) {
+            break
+          }
+          else {
+            const card = game.aChooseCard(player, choices)
+            if (card) {
+              game.aTransfer(player, card, scoreZone)
+            }
+            else {
+              console.log({
+                template: 'Unable to transfer card. Stopping.'
+              })
+              break
+            }
+          }
+        }
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
