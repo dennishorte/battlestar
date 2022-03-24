f115f41f757c4435ecca155eb8ccdf2dd4d73db7
Dennis Horte <dennis@pinterest.com>
2022-03-24 09:13:57 -0700

    Frigate Constitution

--- common/innovation/res/arti/FrigateConstitution.js
+++ common/innovation/res/arti/FrigateConstitution.js
@@ -15,7 +15,31 @@ function Card() {
     `I compel you to reveal a card in your hand! If you do, and its value is equal to the value of any of my top cards, return it and all cards of its color from your board.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player, { leader }) => {
+      const card = game.aChooseCard(player, game.getCardsByZone(player, 'hand'))
+
+      if (card) {
+        game.mReveal(player, card)
+
+        const matchingAges = game
+          .getTopCards(leader)
+          .filter(other => other.getAge() === card.age)
+
+        if (matchingAges.length === 0) {
+          game.mLog({
+            template: "Card age does not match any of {player}'s top cards",
+            args: { player }
+          })
+        }
+        else {
+          const toReturn = game.getCardsByZone(player, card.color)
+          toReturn.push(card)
+          game.aReturnMany(player, toReturn)
+        }
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
