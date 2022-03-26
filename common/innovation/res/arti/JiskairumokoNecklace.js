c69612a662ba4081bdc88ff2e0102726f4875e65
Dennis Horte <dennis@pinterest.com>
2022-03-26 14:55:59 -0700

    Jiskairumoko Necklace

--- common/innovation/res/arti/JiskairumokoNecklace.js
+++ common/innovation/res/arti/JiskairumokoNecklace.js
@@ -15,7 +15,18 @@ function Card() {
     `I compel you to return a card from your score pile! if you do, transfer an achievement of the same value from your achievements to mine!`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player, { leader }) => {
+      const cards = game.aChooseAndReturn(player, game.getCardsByZone(player, 'score'))
+      if (cards && cards.length > 0) {
+        const card = cards[0]
+        const choices = game
+          .getCardsByZone(player, 'achievements')
+          .filter(ach => ach.age === card.age)
+        game.aChooseAndTransfer(player, choices, game.getZoneByPlayer(leader, 'achievements'))
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
