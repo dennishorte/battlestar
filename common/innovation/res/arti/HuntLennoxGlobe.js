0ad60fc39cc4f144cc641b92efd3116a5e9f9bb6
Dennis Horte <dennis@pinterest.com>
2022-03-26 11:05:00 -0700

    Hunt-Lennox Globe

--- common/innovation/res/arti/HuntLennoxGlobe.js
+++ common/innovation/res/arti/HuntLennoxGlobe.js
@@ -12,10 +12,30 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `If you ahve fewer than four cards in your hand, retuan all non-green top cards from your board. Draw a {5} for card returned. Meld a card from your hand.`
+    `If you have fewer than four cards in your hand, return all non-green top cards from your board. Draw a {5} for each card returned. Meld a card from your hand.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      if (game.getCardsByZone(player, 'hand').length < 4) {
+        game.mLog({ template: 'Player has fewer than 4 cards in hand.' })
+        const toReturn = game
+          .getTopCards(player)
+          .filter(card => card.color !== 'green')
+        const returned = game.aReturnMany(player, toReturn)
+        if (returned) {
+          for (let i = 0; i < returned.length; i++) {
+            game.aDraw(player, { age: game.getEffectAge(this, 5) })
+          }
+        }
+      }
+      else {
+        game.mLog({ template: 'Player has 4 or more cards in hand' })
+      }
+
+      game.aChooseAndMeld(player, game.getCardsByZone(player, 'hand'))
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
