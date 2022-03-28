2f92c24610604f2dc652325ac3ac3d73d2c01c08
Dennis Horte <dennis@pinterest.com>
2022-03-27 17:57:33 -0700

    Kilogram of the Archives

--- common/innovation/res/arti/KilogramoftheArchives.js
+++ common/innovation/res/arti/KilogramoftheArchives.js
@@ -15,7 +15,22 @@ function Card() {
     `Return a card from your hand. Return a top card from your board. If you returned two cards and their values sum to ten, draw and score a {0}.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const hand = game.aChooseAndReturn(player, game.getCardsByZone(player, 'hand'))
+      const top = game.aChooseAndReturn(player, game.getTopCards(player))
+
+      if (hand && top && hand.length > 0 && top.length > 0) {
+        const sum = hand[0].getAge() + top[0].getAge()
+        if (sum === 10) {
+          game.aDrawAndScore(player, game.getEffectAge(this, 10))
+        }
+        else {
+          game.mLog({ template: 'Card values do not sum to 10' })
+        }
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
