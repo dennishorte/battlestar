ff1f08ef403689c0598e05e15e74ed35c572ac90
Dennis Horte <dennis@pinterest.com>
2022-04-01 12:39:07 -0700

    Parnell Pitch Drop

--- common/innovation/res/arti/ParnellPitchDrop.js
+++ common/innovation/res/arti/ParnellPitchDrop.js
@@ -1,4 +1,5 @@
 const CardBase = require(`../CardBase.js`)
+const { GameOverEvent } = require('../../game.js')
 
 function Card() {
   this.id = `Parnell Pitch Drop`  // Card names are unique in Innovation
@@ -15,7 +16,17 @@ function Card() {
     `Draw and meld a card of value one higher than the highest top card on your board. If the melded card has three {i}, you win.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const card = game.aDrawAndMeld(player, game.getHighestTopAge(player) + 1)
+      if (card.biscuits.split('i').length - 1 === 3) {
+        throw new GameOverEvent({
+          player,
+          reason: this.name
+        })
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
