ee1d33428842ad67e652db17b40b58dc68749151
Dennis Horte <dennis@pinterest.com>
2022-04-01 11:38:59 -0700

    Ocean Liner Titanic

--- common/innovation/res/arti/OceanLinerTitanic.js
+++ common/innovation/res/arti/OceanLinerTitanic.js
@@ -15,7 +15,19 @@ function Card() {
     `Score all bottom cards from your board.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const toScore = []
+      for (const color of game.utilColors()) {
+        const cards = game.getCardsByZone(player, color)
+        if (cards.length > 0) {
+          toScore.push(cards[cards.length - 1])
+        }
+      }
+
+      game.aScoreMany(player, toScore)
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
