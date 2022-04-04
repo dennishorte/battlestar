e93159e3f97f5fc6f6429e3c856f7ab82589e011
Dennis Horte <dennis@pinterest.com>
2022-04-04 11:07:16 -0700

    Roundhay Garden Scene

--- common/innovation/res/arti/RoundhayGardenScene.js
+++ common/innovation/res/arti/RoundhayGardenScene.js
@@ -15,7 +15,18 @@ function Card() {
     `Meld the highest card from your score pile. Draw and score two cards of value equal to the melded card. Execute the effects of the melded card as if they were on this card. Do not share them.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const choices = game.utilHighestCards(game.getCardsByZone(player, 'score'))
+      const cards = game.aChooseAndMeld(player, choices)
+      if (cards && cards.length > 0) {
+        const card = cards[0]
+        game.aDrawAndScore(player, card.getAge())
+        game.aDrawAndScore(player, card.getAge())
+        game.aExecuteAsIf(player, card)
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
