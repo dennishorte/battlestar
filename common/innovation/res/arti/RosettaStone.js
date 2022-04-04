a710ffc4088baca557217b4a1c2c81981806a36f
Dennis Horte <dennis@pinterest.com>
2022-04-04 10:55:19 -0700

    Rosetta Stone

--- common/innovation/res/arti/RosettaStone.js
+++ common/innovation/res/arti/RosettaStone.js
@@ -12,10 +12,27 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `Choose a card type. Draw two {2} of that type. Meld on and transfer the other to an opponent's board.`
+    `Choose a card type. Draw two {2} of that type. Meld one and transfer the other to an opponent's board.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const exp = game.aChoose(player, game.getExpansionList(), { title: 'Choose a card type' })
+      const cards = [
+        game.aDraw(player, { age: game.getEffectAge(this, 2), exp }),
+        game.aDraw(player, { age: game.getEffectAge(this, 2), exp }),
+      ].filter(card => card !== undefined)
+
+      const card = game.aChooseCard(player, cards, { title: 'Choose a card to meld' })
+      game.aMeld(player, card)
+
+      const otherCard = cards.filter(other => other !== card)[0]
+      if (otherCard) {
+        const opponent = game.aChoosePlayer(player, game.getPlayerOpponents(player))
+        game.aTransfer(player, otherCard, game.getZoneByPlayer(opponent, otherCard.color))
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
---
+++ common/innovation/res/arti/RosettaStone.test.js
@@ -0,0 +1,35 @@
+Error.stackTraceLimit = 100
+
+const t = require('../../testutil.js')
+
+describe("Rosetta Stone", () => {
+
+  test('dogma', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'arti'] })
+    t.setBoard(game,  {
+      dennis: {
+        artifact: ["Rosetta Stone"],
+      },
+      decks: {
+        arti: {
+          2: ['Holy Lance', 'Baghdad Battery'],
+        }
+      }
+    })
+
+    const request1 = game.run()
+    const request2 = t.choose(game, request1, 'dogma')
+    const request3 = t.choose(game, request2, 'arti')
+    const request4 = t.choose(game, request3, 'Baghdad Battery')
+
+    t.testIsFirstAction(request4)
+    t.testBoard(game, {
+      dennis: {
+        green: ['Baghdad Battery'],
+      },
+      micah: {
+        purple: ['Holy Lance'],
+      },
+    })
+  })
+})
