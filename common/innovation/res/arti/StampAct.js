0f731449925e67d692729108f444cbd1da02a277
Dennis Horte <dennis@pinterest.com>
2022-04-06 14:27:24 -0700

    Stamp Act

--- common/innovation/res/arti/StampAct.js
+++ common/innovation/res/arti/StampAct.js
@@ -12,10 +12,34 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `I compel you to transfer a card of value equal to the top yellow card on your board from your score pile to mine! if you do, return a card from your score pile of value equal to the top green card on your board!`
+    `I compel you to transfer a card of value equal to the top yellow card on your board from your score pile to mine! If you do, return a card from your score pile of value equal to the top green card on your board!`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player, { leader }) => {
+      const topYellow = game.getTopCard(player, 'yellow')
+      if (topYellow) {
+        const choices = game
+          .getCardsByZone(player, 'score')
+          .filter(card => card.getAge() === topYellow.getAge())
+        const transferred =
+          game.aChooseAndTransfer(player, choices, game.getZoneByPlayer(leader, 'score'))
+
+        if (transferred && transferred.length > 0) {
+          const topGreen = game.getTopCard(player, 'green')
+          if (topGreen) {
+            const greenChoices = game
+              .getCardsByZone(player, 'score')
+              .filter(card => card.getAge() === topGreen.getAge())
+            game.aChooseAndReturn(player, greenChoices)
+          }
+        }
+        else {
+          game.mLog({ template: 'No card was transferred' })
+        }
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
