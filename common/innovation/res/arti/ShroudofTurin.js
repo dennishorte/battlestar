886be59fbe4a40e28b36dfd158e15532ebaf9696
Dennis Horte <dennis@pinterest.com>
2022-04-06 11:27:59 -0700

    Shroud of Turin

--- common/innovation/res/arti/ShroudofTurin.js
+++ common/innovation/res/arti/ShroudofTurin.js
@@ -15,7 +15,29 @@ function Card() {
     `Return a card from your hand. If you do, return a top card from your board and a card from your score pile of the returned card's color. If you did all three, claim an achievement ignoring eligibility.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const returned = game.aChooseAndReturn(player, game.getCardsByZone(player, 'hand'))
+      if (returned && returned.length > 0) {
+        let totalReturned = 1
+
+        const card = returned[0]
+        const top = game.getTopCard(player, card.color)
+        if (top && game.aReturn(player, top)) {
+          totalReturned += 1
+        }
+
+        const score = game.aChooseAndReturn(player, game.getCardsByZone(player, 'score'))
+        if (score && score.length > 0) {
+          totalReturned += 1
+        }
+
+        if (totalReturned === 3) {
+          game.aChooseAndAchieve(player, game.getAvailableAchievementsRaw(player))
+        }
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
