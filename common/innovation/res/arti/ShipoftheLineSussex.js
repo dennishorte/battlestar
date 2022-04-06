c511e5c5240427c7c005bc18d71998c2e7d7f206
Dennis Horte <dennis@pinterest.com>
2022-04-06 09:17:12 -0700

    Ship of the Line Sussex

--- common/innovation/res/arti/ShipoftheLineSussex.js
+++ common/innovation/res/arti/ShipoftheLineSussex.js
@@ -15,7 +15,19 @@ function Card() {
     `If you have no cards in your score pile, choose a color and score all cards of that color from your board. Otherwise, return all cards from your score pile.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      if (game.getCardsByZone(player, 'score').length === 0) {
+        const colors = game.aChoose(player, game.utilColors(), { title: 'Choose a Color' })
+        const color = colors[0]
+        game.aScoreMany(player, game.getCardsByZone(player, color))
+      }
+
+      else {
+        game.aReturnMany(player, game.getCardsByZone(player, 'score'))
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
