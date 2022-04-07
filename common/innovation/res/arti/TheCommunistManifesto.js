8215bd488a4c1f8fc08f41264721aae1a70af459
Dennis Horte <dennis@pinterest.com>
2022-04-07 11:07:02 -0700

    The Communist Manifesto

--- common/innovation/res/arti/TheCommunistManifesto.js
+++ common/innovation/res/arti/TheCommunistManifesto.js
@@ -15,7 +15,29 @@ function Card() {
     `For each player in the game, draw and reveal a {7}. Transfer one of the drawn cards to each player's board. Execute the non-demand dogma effects of your card. Do not share them.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const players = game.getPlayersStarting(player)
+      const cards = players.map(_ => game.aDrawAndReveal(player, game.getEffectAge(this, 7)))
+
+      let remaining = cards
+      let mine = null
+      for (const target of players) {
+        const title = `Choose a card to transfer to ${target.name}`
+        const card = game.aChooseCard(player, remaining, { title })
+        remaining = remaining.filter(other => other !== card)
+
+        const transferred = game.aTransfer(player, card, game.getZoneByPlayer(target, card.color))
+        if (transferred && target === player) {
+          mine = card
+        }
+      }
+
+      if (mine) {
+        game.aCardEffects(player, mine, 'dogma')
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
