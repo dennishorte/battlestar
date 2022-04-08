2d3110e5faff1d32088de7e0226adfaa726a2acb
Dennis Horte <dennis@pinterest.com>
2022-04-08 08:21:45 -0700

    The Daily Courant

--- common/innovation/res/arti/TheDailyCourant.js
+++ common/innovation/res/arti/TheDailyCourant.js
@@ -15,7 +15,25 @@ function Card() {
     `Draw a card of any value, then place it on top of the draw pile of its age. Execute the effects of one of your other top cards as if they were on this card. Do not share them.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const age = game.aChooseAge(player)
+      const card = game.aDraw(player, { age })
+      if (card) {
+        game.mMoveCardToTop(card, game.getZoneByCardHome(card))
+        game.mLog({
+          template: '{player} puts {card} on back on top of deck',
+          args: { player, card }
+        })
+      }
+
+      const choices = game.getTopCards(player)
+      const toExecute = game.aChooseCard(player, choices)
+      if (toExecute) {
+        game.aExecuteAsIf(player, toExecute)
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
