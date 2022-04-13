5540fda6d85d071fbd31cfb045878cccdf12cf5b
Dennis Horte <dennis@pinterest.com>
2022-04-13 13:34:26 -0700

    Almanac

--- common/innovation/res/echo/Almanac.js
+++ common/innovation/res/echo/Almanac.js
@@ -12,11 +12,26 @@ function Card() {
   this.echo = `Draw and foreshadow a {4}.`
   this.karma = []
   this.dogma = [
-    `You may return a card from your forecast with a bonus. If you do, draw and score a card of value on higher than that bonus.`
+    `You may return a card from your forecast with a bonus. If you do, draw and score a card of value one higher than that bonus.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const choices = game
+        .getCardsByZone(player, 'forecast')
+        .filter(card => card.checkHasBonus())
+
+      const cards = game.aChooseAndReturn(player, choices, { min: 0, max: 1 })
+      if (cards && cards.length > 0) {
+        const card = cards[0]
+        const bonuses = card.getBonuses()
+        game.aDrawAndScore(player, bonuses[0] + 1)
+      }
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    game.aDrawAndForeshadow(player, game.getEffectAge(this, 4))
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
