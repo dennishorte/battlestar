02af18927fff1f923692b192f00cffd94f029969
Dennis Horte <dennis@pinterest.com>
2022-04-14 14:43:30 -0700

    Bandage

--- common/innovation/res/echo/Bandage.js
+++ common/innovation/res/echo/Bandage.js
@@ -15,8 +15,29 @@ function Card() {
     `I demand you return the highest card in your score pile for which you do not have a card of matching value in your hand! Return a top card from your board with a {i}!`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const handAges = game
+        .getCardsByZone(player, 'hand')
+        .map(card => card.getAge())
+      const choices = game
+        .getCardsByZone(player, 'score')
+        .filter(card => !handAges.includes(card.getAge()))
+      const highest = game.utilHighestCards(choices)
+      game.aChooseAndReturn(player, highest)
+
+      const boardChoices = game
+        .getTopCards(player)
+        .filter(card => card.checkHasBiscuit('i'))
+      game.aChooseAndReturn(player, boardChoices)
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    const choices = game
+      .getCardsByZone(player, 'hand')
+      .filter(card => card.checkHasBiscuit('l'))
+    game.aChooseAndMeld(player, choices)
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
