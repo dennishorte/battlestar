61a2db1a66ff4fa4f2f46cbe63caa656c469beae
Dennis Horte <dennis@pinterest.com>
2022-04-15 07:36:23 -0700

    Mask of Warka

--- common/innovation/res/arti/MaskofWarka.js
+++ common/innovation/res/arti/MaskofWarka.js
@@ -15,7 +15,34 @@ function Card() {
     `Choose a color. Each player reveals all cards of that color from their hand. If you are the only player to reveal cards, return them and claim all achievements of value matching those cards, ignoring eligibility.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const color = game.aChoose(player, game.utilColors(), { title: 'Choose a Color' })[0]
+      const revealedBy = []
+
+      for (const plyr of game.getPlayersStarting(player)) {
+        const toReveal = game
+          .getCardsByZone(plyr, 'hand')
+          .filter(card => card.color === color)
+        const revealed = game.aRevealMany(player, toReveal, { ordered: true })
+        if (revealed && revealed.length > 0) {
+          revealedBy.push(plyr)
+        }
+      }
+
+      if (revealedBy.length === 1 && revealedBy[0] === player) {
+        const toReturn = game
+          .getCardsByZone(player, 'hand')
+          .filter(card => card.color === color)
+        const returned = game.aReturnMany(player, toReturn)
+        const toClaim = toReturn.map(card => card.getAge())
+        const available = game
+          .getAvailableAchievementsRaw(player)
+          .filter(card => toClaim.includes(card.getAge()))
+          .forEach(card => game.aClaimAchievement(player, { card }))
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
