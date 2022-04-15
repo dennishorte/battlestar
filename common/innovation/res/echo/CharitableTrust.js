b828d2f10924d0602b7cc829aa9addf049f21c2b
Dennis Horte <dennis@pinterest.com>
2022-04-15 13:21:13 -0700

    Charitable Trust

--- common/innovation/res/echo/CharitableTrust.js
+++ common/innovation/res/echo/CharitableTrust.js
@@ -15,8 +15,37 @@ function Card() {
     `You may meld the card you drew due to Charitable Trust's echo effect. If you do, either return or achieve (if eligible) your top green card.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const card = game.state.dogmaInfo.charitableTrust[player.name]
+      if (card) {
+        const meldIt = game.aYesNo(player, `Meld ${card.name}?`)
+        if (meldIt) {
+          game.aMeld(player, card)
+
+          const greenCard = game.getTopCard(player, 'green')
+          if (greenCard) {
+            if (game.checkAchievementEligibility(player, greenCard)) {
+              game.aClaimAchievement(player, { card: greenCard })
+            }
+            else {
+              game.aReturn(player, greenCard)
+            }
+          }
+        }
+      }
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    const age = game.aChooseAge(player, [game.getEffectAge(this, 3), game.getEffectAge(this, 4)])
+    const card = game.aDraw(player, { age })
+
+    if (!game.state.dogmaInfo.charitableTrust) {
+      game.state.dogmaInfo.charitableTrust = {}
+    }
+
+    game.state.dogmaInfo.charitableTrust[player.name] = card
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
