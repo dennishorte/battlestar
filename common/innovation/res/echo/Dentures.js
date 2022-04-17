83b54ff657d6dc61795b791c53d630b83030092d
Dennis Horte <dennis@pinterest.com>
2022-04-17 13:10:28 -0700

    Dentures

--- common/innovation/res/echo/Dentures.js
+++ common/innovation/res/echo/Dentures.js
@@ -12,12 +12,58 @@ function Card() {
   this.echo = `Draw and tuck a {6}.`
   this.karma = []
   this.dogma = [
-    `Score the top two non-bottom cards of the color of the last card you tucked due to Dentures. If there arre non to score, draw and tuck a {6}, then repeat this dogma effect.`,
+    `Score the top two non-bottom cards of the color of the last card you tucked due to Dentures. If there are none to score, draw and tuck a {6}, then repeat this dogma effect.`,
     `You may splay your blue cards right.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      let card = game.state.dogmaInfo.dentures[player.name]
+
+      while (true) {
+        if (card) {
+          const cards = game.getCardsByZone(player, card.color)
+          game.mLog({
+            template: '{player} will try to score {color}',
+            args: { player, color: card.color }
+          })
+
+          if (cards.length === 1) {
+            game.mLog({
+              template: '{player} has no non-bottom {color} cards',
+              args: { player, color: card.color }
+            })
+            card = game.aDrawAndTuck(player, game.getEffectAge(this, 6))
+            continue
+          }
+          if (cards.length > 1) {
+            game.aScore(player, cards[0])
+          }
+          if (cards.length > 2) {
+            game.aScore(player, cards[1])
+          }
+          break
+        }
+        else {
+          game.mLogNoEffect()
+          break
+        }
+      }
+    },
+
+    (game, player) => {
+      game.aChooseAndSplay(player, ['blue'], 'up')
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    const card = game.aDrawAndTuck(player, game.getEffectAge(this, 6))
+
+    if (!game.state.dogmaInfo.dentures) {
+      game.state.dogmaInfo.dentures = {}
+    }
+
+    game.state.dogmaInfo.dentures[player.name] = card
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
