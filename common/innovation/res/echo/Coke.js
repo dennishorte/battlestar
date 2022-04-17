6890b60842ceac78eb994fdcc437bd9750a254ef
Dennis Horte <dennis@pinterest.com>
2022-04-17 09:14:59 -0700

    Coke

--- common/innovation/res/echo/Coke.js
+++ common/innovation/res/echo/Coke.js
@@ -15,8 +15,31 @@ function Card() {
     `Draw and reveal a {6}. If it has a {f}, meld it and repeat this dogma effect. Otherwise, foreshadow it.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      while (true) {
+        const card = game.aDrawAndReveal(player, game.getEffectAge(this, 6))
+        if (card) {
+          if (card.checkHasBiscuit('f')) {
+            game.mLog({ template: 'Card has {f}.' })
+            game.aMeld(player, card)
+            continue
+          }
+          else {
+            game.mLog({ template: 'Card did not have {f}.' })
+            game.aForeshadow(player, card)
+            break
+          }
+        }
+        else {
+          break
+        }
+      }
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    game.aDrawAndTuck(player, game.getEffectAge(this, 4))
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
