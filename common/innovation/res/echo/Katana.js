d2b7cef240d7d28bfa5783b57a03a53df86de0a9
Dennis Horte <dennis@pinterest.com>
2022-04-25 11:35:45 -0700

    Katana

--- common/innovation/res/echo/Katana.js
+++ common/innovation/res/echo/Katana.js
@@ -12,10 +12,29 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `I demand you transfer two top cards with a {k} from your board to my score pile! If you transferred any, draw a card of value qual to the total number of {k} on those cards and transfer it to my forecast!`
+    `I demand you transfer two top cards with a {k} from your board to my score pile! If you transferred any, draw a card of value equal to the total number of {k} on those cards and transfer it to my forecast!`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player, { leader }) => {
+      const choices = game
+        .getTopCards(player)
+        .filter(card => card.checkHasBiscuit('k'))
+      const transferred = game.aChooseAndTransfer(player, choices, game.getZoneByPlayer(leader, 'score'), { count: 2 })
+
+      if (transferred && transferred.length > 0) {
+        const castleCount = transferred
+          .flatMap(card => card.biscuits.split(''))
+          .filter(biscuit => biscuit === 'k')
+          .length
+
+        const card = game.aDraw(player, { age: castleCount })
+        if (card) {
+          game.aTransfer(player, card, game.getZoneByPlayer(leader, 'forecast'))
+        }
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
