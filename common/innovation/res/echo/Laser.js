2d59c2d60437f671e1ad4d2913867097f232948f
Dennis Horte <dennis@pinterest.com>
2022-04-26 16:11:20 -0700

    Laser

--- common/innovation/res/echo/Laser.js
+++ common/innovation/res/echo/Laser.js
@@ -15,7 +15,22 @@ function Card() {
     `Return all unclaimed standard achievements. Then, return half (rounded up) of the cards in your score pile. Draw and meld two {0}.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const toReturn = game
+        .getZoneById('achievements')
+        .cards()
+        .filter(card => !card.isSpecialAchievement)
+      game.aReturnMany(player, toReturn, { ordered: true })
+
+      const score = game.getCardsByZone(player, 'score')
+      const returnCount = Math.ceil(score.length / 2)
+      game.aChooseAndReturn(player, score, { count: returnCount })
+
+      game.aDrawAndMeld(player, game.getEffectAge(this, 10))
+      game.aDrawAndMeld(player, game.getEffectAge(this, 10))
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
