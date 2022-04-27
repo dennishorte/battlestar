81f5a64a02f48547db3c9db308828a53695ffd1f
Dennis Horte <dennis@pinterest.com>
2022-04-27 08:30:04 -0700

    Liquid Fire

--- common/innovation/res/echo/LiquidFire.js
+++ common/innovation/res/echo/LiquidFire.js
@@ -15,7 +15,27 @@ function Card() {
     `I demand you draw and reveal a card of value equal to the highest bonus on your board! Transfer it to my forecast! If it is red, transfer all cards from your hand to my score pile.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player, { leader }) => {
+      const age = Math.max(...game.getBonuses(player))
+      if (age && age > 0) {
+        const card = game.aDrawAndReveal(player, age)
+        if (card) {
+          game.aTransfer(player, card, game.getZoneByPlayer(leader, 'forecast'))
+
+          if (card.color === 'red') {
+            game.aTransferMany(player, game.getCardsByZone(player, 'hand'), game.getZoneByPlayer(leader, 'score'))
+          }
+        }
+      }
+      else {
+        game.mLog({
+          template: '{player} has no bonuses',
+          args: { player }
+        })
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
