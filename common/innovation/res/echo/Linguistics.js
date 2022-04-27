d372708e74e7583d2831e33256567ff2ac19447d
Dennis Horte <dennis@pinterest.com>
2022-04-27 08:13:24 -0700

    Linguistics

--- common/innovation/res/echo/Linguistics.js
+++ common/innovation/res/echo/Linguistics.js
@@ -1,4 +1,5 @@
 const CardBase = require(`../CardBase.js`)
+const util = require('../../../lib/util.js')
 
 function Card() {
   this.id = `Linguistics`  // Card names are unique in Innovation
@@ -15,8 +16,29 @@ function Card() {
     `Draw a card of value equal to a bonus on your board, if you have any.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const bonuses = util.array.distinct(game.getBonuses(player)).sort()
+      const age = game.aChooseAge(player, bonuses, { title: 'Choose an age to draw from' })
+      if (age) {
+        game.aDraw(player, { age })
+      }
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    const choices = [
+      `Draw a ${game.getEffectAge(this, 3)}`,
+      `Draw and foreshadow a ${game.getEffectAge(this, 4)}`,
+    ]
+    const choice = game.aChoose(player, choices)[0]
+
+    if (choice.includes('foreshadow')) {
+      game.aDrawAndForeshadow(player, game.getEffectAge(this, 4))
+    }
+    else {
+      game.aDraw(player, { age: game.getEffectAge(this, 3) })
+    }
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
