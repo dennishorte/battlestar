704905e35ce21424d275b19eb4770834d65d58dd
Dennis Horte <dennis@pinterest.com>
2022-04-28 10:03:55 -0700

    Morphine

--- common/innovation/res/echo/Morphine.js
+++ common/innovation/res/echo/Morphine.js
@@ -13,12 +13,48 @@ function Card() {
   this.karma = []
   this.dogma = [
     `I demand you return all odd-valued cards in your hand! Draw a {6}!`,
-    `Draw a card of value on higher than the highest card returned due to the demand, if any were returned.`,
+    `Draw a card of value one higher than the highest card returned due to the demand, if any were returned.`,
     `You may splay your red cards right.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const toReturn = game
+        .getCardsByZone(player, 'hand')
+        .filter(card => card.getAge() % 2 === 1)
+      const returned = game.aReturnMany(player, toReturn)
+      game.aDraw(player, { age: game.getEffectAge(this, 6) })
+
+      if (!game.state.dogmaInfo.morphine) {
+        game.state.dogmaInfo.morphine = 0
+      }
+      if (returned) {
+        game.state.dogmaInfo.morphine = Math.max(
+          game.state.dogmaInfo.morphine,
+          ...returned.map(card => card.getAge())
+        )
+      }
+    },
+
+    (game, player) => {
+      if (game.state.dogmaInfo.morphine) {
+        game.aDraw(player, { age: game.state.dogmaInfo.morphine + 1 })
+      }
+      else {
+        game.mLog({ template: 'No cards were returned due to the demand.' })
+      }
+    },
+
+    (game, player) => {
+      game.aChooseAndSplay(player, ['red'], 'right')
+    },
+  ]
+  this.echoImpl = (game, player) => {
+    const choices = game
+      .getCardsByZone(player, 'hand')
+      .filter(card => card.getAge() % 2 === 1)
+    game.aChooseAndScore(player, choices)
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
