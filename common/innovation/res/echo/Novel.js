dc1df4df0fa0c63a4990b9e8bcf45486c284309b
Dennis Horte <dennis@pinterest.com>
2022-04-29 09:29:51 -0700

    Novel

--- common/innovation/res/echo/Novel.js
+++ common/innovation/res/echo/Novel.js
@@ -16,8 +16,41 @@ function Card() {
     `If all your non-purple top cards share a common icon other than {c}, claim the Supremacy achievement.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      game.aDraw(player, { age: game.getEffectAge(this, 3) })
+      game.aChooseAndSplay(player, ['purple'], 'left')
+    },
+
+    (game, player) => {
+      if (!game.checkAchievementAvailable('Supremacy')) {
+        game.mLogNoEffect()
+        return
+      }
+
+      const biscuits = game
+        .getTopCards(player)
+        .filter(card => card.color !== 'purple')
+        .map(card => card.biscuits)
+
+      if (biscuits.length === 0) {
+        game.mLogNoEffect()
+        return
+      }
+
+      for (const biscuit of Object.keys(game.utilEmptyBiscuits())) {
+        if (biscuits.every(b => b.includes(biscuit))) {
+          game.aClaimAchievement(player, { name: 'Supremacy' })
+          return
+        }
+      }
+
+      game.mLogNoEffect()
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    game.aDraw(player, { age: game.getEffectAge(this, 3) })
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
