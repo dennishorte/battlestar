61ca98e79cf542eeb6d41d147bfacd81f9a11d0f
Dennis Horte <dennis@pinterest.com>
2022-05-01 07:49:37 -0700

    Piano

--- common/innovation/res/echo/Piano.js
+++ common/innovation/res/echo/Piano.js
@@ -1,4 +1,5 @@
 const CardBase = require(`../CardBase.js`)
+const util = require('../../../lib/util.js')
 
 function Card() {
   this.id = `Piano`  // Card names are unique in Innovation
@@ -15,8 +16,48 @@ function Card() {
     `If you have five top cards, each with a different value, return five cards from your score pile and then draw and score a card of each of your top cards' values in ascending order.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const topCards = game.getTopCards(player)
+
+      if (topCards.length === 5) {
+        const values = util.array.distinct(topCards.map(card => card.getAge())).sort()
+
+        if (values.length === 5) {
+          game.aChooseAndReturn(player, game.getCardsByZone(player, 'score'), { count: 5 })
+          for (const age of values) {
+            game.aDrawAndScore(player, age)
+          }
+        }
+
+        else {
+          game.mLog({
+            template: '{player} top cards do not have all different values',
+            args: { player }
+          })
+        }
+      }
+
+      else {
+        game.mLog({
+          template: '{player} does not have five top cards',
+          args: { player }
+        })
+      }
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    const ages = game
+      .getPlayerAll()
+      .flatMap(player => game.getCardsByZone(player, 'hand'))
+      .map(card => card.getAge())
+      .sort()
+    const choices = util.array.distinct(ages)
+    const age = game.aChooseAge(player, choices, { title: 'Choose an age to draw from' })
+    if (age) {
+      game.aDraw(player, { age })
+    }
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
