cd4c5d736a4f89bd599385845df6fe01823ba639
Dennis Horte <dennis@pinterest.com>
2022-05-05 11:57:08 -0700

    Television

--- common/innovation/res/echo/Television.js
+++ common/innovation/res/echo/Television.js
@@ -15,8 +15,35 @@ function Card() {
     `Choose a value and and opponent. Transfer a card of that value from their score pile to their board. If they have an achievement of the same value, achieve (if eligible) a card of that value from their score pile.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      const opponent = game.aChoosePlayer(player, game.getPlayerOpponents(player))
+      const age = game.aChooseAge(player)
+
+      const choices = game
+        .getCardsByZone(opponent, 'score')
+        .filter(card => card.getAge() === age)
+      const card = game.aChooseCard(player, choices)
+      if (card) {
+        game.aTransfer(player, card, game.getZoneByPlayer(opponent, card.color))
+
+        const matchingAchievements = game
+          .getCardsByZone(opponent, 'achievements')
+          .filter(card => card.getAge() === card.age)
+
+        if (matchingAchievements.length > 0) {
+          const achieveChoices = game
+            .getCardsByZone(opponent, 'score')
+            .filter(card => card.getAge() === age)
+            .filter(card => game.checkAchievementEligibility(player, card))
+          game.aChooseAndAchieve(player, achieveChoices)
+        }
+      }
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    game.aDrawAndMeld(player, game.getEffectAge(this, 8))
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
