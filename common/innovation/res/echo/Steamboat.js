83ea733908357817cc5ac66e114fef74092b9828
Dennis Horte <dennis@pinterest.com>
2022-05-05 08:02:30 -0700

    Steamboat

--- common/innovation/res/echo/Steamboat.js
+++ common/innovation/res/echo/Steamboat.js
@@ -15,7 +15,26 @@ function Card() {
     `I demand you draw and reveal a {6}! If it is blue or yellow, transfer it and all cards in your hand to my hand! If it is red or green, keep it and transfer two cards from your score pile to mine! If it is purple, keep it!`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player, { leader }) => {
+      const card = game.aDrawAndReveal(player, game.getEffectAge(this, 6))
+      if (card) {
+        if (card.color === 'blue' || card.color === 'yellow') {
+          game.aTransferMany(player, game.getCardsByZone(player, 'hand'), game.getZoneByPlayer(leader, 'hand'))
+        }
+
+        else if (card.color === 'red' || card.color === 'green') {
+          game.aChooseAndTransfer(player, game.getCardsByZone(player, 'score'), game.getZoneByPlayer(leader, 'score'), { count: 2 })
+        }
+
+        else {
+          game.mLog({
+            template: 'Card was purple. No effect.'
+          })
+        }
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
---
+++ common/innovation/res/echo/Steamboat.test.js
@@ -0,0 +1,103 @@
+Error.stackTraceLimit = 100
+
+const t = require('../../testutil.js')
+
+describe("Steamboat", () => {
+
+  test('dogma: blue or yellow', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        green: ['Steamboat'],
+      },
+      micah: {
+        hand: ['Candles'],
+        score: ['Sailing'],
+      },
+      decks: {
+        base: {
+          6: ['Atomic Theory']
+        },
+      },
+    })
+
+    const request1 = game.run()
+    const request2 = t.choose(game, request1, 'Dogma.Steamboat')
+    const request3 = t.choose(game, request2, 'auto')
+
+    t.testIsSecondPlayer(request3)
+    t.testBoard(game, {
+      dennis: {
+        green: ['Steamboat'],
+        hand: ['Candles', 'Atomic Theory'],
+      },
+      micah: {
+        score: ['Sailing'],
+      },
+    })
+  })
+
+  test('dogma: red or green', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        green: ['Steamboat'],
+      },
+      micah: {
+        hand: ['Candles'],
+        score: ['Sailing'],
+      },
+      decks: {
+        base: {
+          6: ['Classification']
+        },
+      },
+    })
+
+    const request1 = game.run()
+    const request2 = t.choose(game, request1, 'Dogma.Steamboat')
+
+    t.testIsSecondPlayer(request2)
+    t.testBoard(game, {
+      dennis: {
+        green: ['Steamboat'],
+        score: ['Sailing'],
+      },
+      micah: {
+        hand: ['Candles', 'Classification'],
+      },
+    })
+  })
+
+  test('dogma: purple', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        green: ['Steamboat'],
+      },
+      micah: {
+        hand: ['Candles'],
+        score: ['Sailing'],
+      },
+      decks: {
+        base: {
+          6: ['Democracy']
+        },
+      },
+    })
+
+    const request1 = game.run()
+    const request2 = t.choose(game, request1, 'Dogma.Steamboat')
+
+    t.testIsSecondPlayer(request2)
+    t.testBoard(game, {
+      dennis: {
+        green: ['Steamboat'],
+      },
+      micah: {
+        hand: ['Candles', 'Democracy'],
+        score: ['Sailing'],
+      },
+    })
+  })
+})
