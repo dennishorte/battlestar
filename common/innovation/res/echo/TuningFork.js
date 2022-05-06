2d0fd11f2257225c6e01b3745b798a3cd265aca1
Dennis Horte <dennis@pinterest.com>
2022-05-06 08:55:07 -0700

    Tuning Fork

--- common/innovation/res/echo/TuningFork.js
+++ common/innovation/res/echo/TuningFork.js
@@ -15,8 +15,49 @@ function Card() {
     `Return a card from your hand. If you do, draw and reveal a card of the same value, and meld it if it is of higher value than the top card of the same color on your board. Otherwise, return it. You may repeat this dogma effect.`
   ]
 
-  this.dogmaImpl = []
-  this.echoImpl = []
+  this.dogmaImpl = [
+    (game, player) => {
+      while (true) {
+        const returned = game.aChooseAndReturn(player, game.getCardsByZone(player, 'hand'))[0]
+        if (returned) {
+          const revealed = game.aDrawAndReveal(player, returned.getAge())
+          if (revealed) {
+            const top = game.getTopCard(player, revealed.color)
+            if (!top || revealed.getAge() > top.getAge()) {
+              game.aMeld(player, revealed)
+            }
+            else {
+              game.aReturn(player, revealed)
+            }
+          }
+        }
+        else {
+          break
+        }
+
+        if (game.getCardsByZone(player, 'hand').length === 0) {
+          break
+        }
+
+        const repeat = game.aYesNo(player, 'Repeat this dogma effect?')
+        if (repeat) {
+          continue
+        }
+        else {
+          break
+        }
+      }
+    }
+  ]
+  this.echoImpl = (game, player) => {
+    const exp = game.aChoose(player, game.getExpansionList(), { title: 'Choose the type of deck' })[0]
+    const ages = [1,2,3,4,5,6,7,8,9,10]
+      .filter(age => game.getZoneByDeck(exp, age).cards().length > 0)
+    const age = game.aChooseAge(player, ages)
+
+    const card = game.mDraw(player, exp, age)
+    game.mMoveCardToTop(card, game.getZoneById(card.home), { player })
+  }
   this.inspireImpl = []
   this.karmaImpl = []
 }
---
+++ common/innovation/res/echo/TuningFork.test.js
@@ -0,0 +1,38 @@
+Error.stackTraceLimit = 100
+
+const t = require('../../testutil.js')
+
+describe("Tuning Fork", () => {
+
+  test('dogma', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        purple: ['Tuning Fork'],
+        green: ['Sailing'],
+        hand: ['Candles', 'Domestication'],
+      },
+      decks: {
+        base: {
+          1: ['Tools', 'The Wheel'],
+        },
+      }
+    })
+
+    const request1 = game.run()
+    const request2 = t.choose(game, request1, 'Dogma.Tuning Fork')
+    const request3 = t.choose(game, request2, 'base')
+    const request4 = t.choose(game, request3, 1)
+    const request5 = t.choose(game, request4, 'Domestication')
+    const request6 = t.choose(game, request5, 'yes')
+
+    t.testIsSecondPlayer(request6)
+    t.testBoard(game, {
+      dennis: {
+        purple: ['Tuning Fork'],
+        green: ['Sailing'],
+        blue: ['Tools'],
+      },
+    })
+  })
+})
