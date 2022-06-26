1ab3a1f73eb38ef4e8965a13861a4d2d53f1e61d
Dennis Horte <dennis@pinterest.com>
2022-06-26 10:14:20 -0700

    Innovation: Dancing Girl total change

--- common/innovation/res/arti/DancingGirl.js
+++ common/innovation/res/arti/DancingGirl.js
@@ -13,42 +13,52 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `I compel you to transfer Dancing Girl to your board!`,
-    `If Dancing Girl has been on every board during this action, and it started on your board, you win.`
+    `I compel you to draw a card of value 2 higher than your highest top card and transfer it to my board.`
+    // `I compel you to transfer Dancing Girl to your board!`,
+    // `If Dancing Girl has been on every board during this action, and it started on your board, you win.`
   ]
 
   this.dogmaImpl = [
-    (game, player) => {
-      if (!game.state.dogmaInfo.dancingGirl) {
-        game.state.dogmaInfo.dancingGirl = {}
-        game.state.dogmaInfo.dancingGirl.zones = []
-        game.state.dogmaInfo.dancingGirl.startingZone = this.zone
-      }
-
-      game.aTransfer(player, this, game.getZoneByPlayer(player, this.color))
-      game.state.dogmaInfo.dancingGirl.zones.push(this.zone) // Zone after transfer
-    },
-
     (game, player, { leader }) => {
-      if (player !== leader) {
-        game.mLogNoEffect()
-        return
-      }
-
-      const info = game.state.dogmaInfo.dancingGirl
-      const startingCondition = info.startingZone === `players.${player.name}.yellow`
-      const travelCondition = info.zones.length === game.getPlayerAll().length - 1
-
-      if (startingCondition && travelCondition) {
-        throw new GameOverEvent({
-          player,
-          reason: this.name
-        })
-      }
-      else {
-        game.mLogNoEffect()
+      const highestCard = game.utilHighestCards(game.getTopCards(player))[0]
+      const age = highestCard ? highestCard.getAge() + 2 : 2
+      const card = game.aDraw(player, { age })
+      if (card) {
+        game.aTransfer(player, card, game.getZoneByPlayer(leader, card.color))
       }
     }
+
+    /* (game, player) => {
+     *   if (!game.state.dogmaInfo.dancingGirl) {
+     *     game.state.dogmaInfo.dancingGirl = {}
+     *     game.state.dogmaInfo.dancingGirl.zones = []
+     *     game.state.dogmaInfo.dancingGirl.startingZone = this.zone
+     *   }
+
+     *   game.aTransfer(player, this, game.getZoneByPlayer(player, this.color))
+     *   game.state.dogmaInfo.dancingGirl.zones.push(this.zone) // Zone after transfer
+     * },
+
+     * (game, player, { leader }) => {
+     *   if (player !== leader) {
+     *     game.mLogNoEffect()
+     *     return
+     *   }
+
+     *   const info = game.state.dogmaInfo.dancingGirl
+     *   const startingCondition = info.startingZone === `players.${player.name}.yellow`
+     *   const travelCondition = info.zones.length === game.getPlayerAll().length - 1
+
+     *   if (startingCondition && travelCondition) {
+     *     throw new GameOverEvent({
+     *       player,
+     *       reason: this.name
+     *     })
+     *   }
+     *   else {
+     *     game.mLogNoEffect()
+     *   }
+     * } */
   ]
   this.echoImpl = []
   this.inspireImpl = []
--- common/innovation/res/arti/DancingGirl.test.js
+++ common/innovation/res/arti/DancingGirl.test.js
@@ -13,34 +13,64 @@ describe('Dancing Girl', () => {
       micah: {
         purple: ['Philosophy'],
       },
+      decks: {
+        base: {
+          4: ['Gunpowder'],
+        }
+      },
     })
 
     const request1 = game.run()
     const request2 = t.choose(game, request1, 'Dogma.Dancing Girl')
 
-    t.testGameOver(request2, 'dennis', 'Dancing Girl')
-  })
-
-  test('dogma: free artifact action does not win', () => {
-    const game = t.fixtureFirstPlayer({ expansions: ['base', 'arti'] })
-    t.setBoard(game,  {
+    t.testBoard(game,  {
       dennis: {
-        artifact: ['Dancing Girl'],
-      },
-      micah: {
-        purple: ['Philosophy'],
-      },
-    })
-
-    const request1 = game.run()
-    const request2 = t.choose(game, request1, 'dogma')
-
-    t.testIsFirstAction(request2)
-    t.testBoard(game, {
-      micah: {
         yellow: ['Dancing Girl'],
+        red: ['Gunpowder'],
+      },
+      micah: {
         purple: ['Philosophy'],
       },
     })
   })
+
+  /* test('dogma', () => {
+   *   const game = t.fixtureFirstPlayer({ expansions: ['base', 'arti'] })
+   *   t.setBoard(game,  {
+   *     dennis: {
+   *       yellow: ['Dancing Girl'],
+   *     },
+   *     micah: {
+   *       purple: ['Philosophy'],
+   *     },
+   *   })
+
+   *   const request1 = game.run()
+   *   const request2 = t.choose(game, request1, 'Dogma.Dancing Girl')
+
+   *   t.testGameOver(request2, 'dennis', 'Dancing Girl')
+   * })
+
+   * test('dogma: free artifact action does not win', () => {
+   *   const game = t.fixtureFirstPlayer({ expansions: ['base', 'arti'] })
+   *   t.setBoard(game,  {
+   *     dennis: {
+   *       artifact: ['Dancing Girl'],
+   *     },
+   *     micah: {
+   *       purple: ['Philosophy'],
+   *     },
+   *   })
+
+   *   const request1 = game.run()
+   *   const request2 = t.choose(game, request1, 'dogma')
+
+   *   t.testIsFirstAction(request2)
+   *   t.testBoard(game, {
+   *     micah: {
+   *       yellow: ['Dancing Girl'],
+   *       purple: ['Philosophy'],
+   *     },
+   *   })
+   * }) */
 })
