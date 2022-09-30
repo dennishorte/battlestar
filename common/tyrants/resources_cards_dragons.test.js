f05afe9e9d6e05d82162028c2a9b3ef08359cc44
Dennis Horte <dennis@pinterest.com>
2022-09-30 09:30:54 -0700

    Tyrants: Dragon Cultist

--- common/tyrants/resources_cards_dragons.test.js
+++ common/tyrants/resources_cards_dragons.test.js
@@ -10,4 +10,46 @@ const t = require('./testutil.js')
 
 describe('Dragons Expansion Cards', () => {
 
+  describe('Dragon Cultist', () => {
+    test('Choose power', () => {
+      const game = t.gameFixture({
+        dennis: {
+          hand: ['Dragon Cultist'],
+        }
+      })
+
+      const request1 = game.run()
+      const request2 = t.choose(game, request1, 'Play Card.Dragon Cultist')
+      const request3 = t.choose(game, request2, '+2 power')
+
+      t.testBoard(game, {
+        dennis: {
+          hand: [],
+          played: ['Dragon Cultist'],
+          power: 2,
+        }
+      })
+    })
+
+    test('Choose influence', () => {
+      const game = t.gameFixture({
+        dennis: {
+          hand: ['Dragon Cultist'],
+        }
+      })
+
+      const request1 = game.run()
+      const request2 = t.choose(game, request1, 'Play Card.Dragon Cultist')
+      const request3 = t.choose(game, request2, '+2 influence')
+
+      t.testBoard(game, {
+        dennis: {
+          hand: [],
+          played: ['Dragon Cultist'],
+          influence: 2,
+        }
+      })
+    })
+  })
+
 })
