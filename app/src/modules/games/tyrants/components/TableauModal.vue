194b57e3d68db8eec4c1399e89cbc86002020b24
Dennis Horte <dennis@pinterest.com>
2022-10-26 21:57:07 -0700

    Tyrants UI: Add trophy hall to tableau view

--- app/src/modules/games/tyrants/components/TableauModal.vue
+++ app/src/modules/games/tyrants/components/TableauModal.vue
@@ -1,5 +1,20 @@
 <template>
   <b-modal id="tableau-modal" scrollable :title="title">
+    <div class="trophyHall">
+      <div class="title">Trophy Hall</div>
+      <div class="trophies" v-if="trophyHall.length > 0">
+        <div
+          v-for="token in trophyHall"
+          :key="token.id"
+          :class="trophyClasses(token)"
+        ></div>
+      </div>
+
+      <div v-else>
+        — No Trophies —
+      </div>
+    </div>
+
     <div class="deck">
       <div class="title">All Cards in Deck</div>
       <GameCard v-for="card in allCards" :key="card.id" :card="card" />
@@ -43,10 +58,49 @@ export default {
     title() {
       return `${this.player.name}'s tableau`
     },
-  }
+
+    trophyHall() {
+      return this.game.getCardsByZone(this.player, 'trophyHall')
+    },
+  },
+
+  methods: {
+    trophyClasses(troop) {
+      const classes = ['trophy']
+
+      if (troop.owner === undefined) {
+        classes.push('neutral-element')
+      }
+      else {
+        const color = this.ui.fn.getPlayerColor(this.game, troop.owner)
+        classes.push(`${color}-element`)
+      }
+
+      return classes
+    },
+  },
 }
 </script>
 
 
 <style scoped>
+.title {
+  font-size: 1.2em;
+  font-weight: 500;
+}
+
+.trophies {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.trophy {
+  height: 1.2em;
+  width: 1.2em;
+  min-width: 1.2em;
+  border-radius: 50%;
+  border: 1px solid black;
+  margin: 1px;
+}
 </style>
