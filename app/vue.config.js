b9d25a24b234530c702ace60523e1260da3fcca1
Dennis Horte <dennis@pinterest.com>
2022-10-31 13:20:27 -0700

    Vue3: Remove vue2 compat mode

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,22 +1,4 @@
 module.exports = {
-  chainWebpack: config => {
-    config.resolve.alias.set('vue', '@vue/compat')
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
-  },
-
   configureWebpack: {
     resolve: {
       symlinks: false
