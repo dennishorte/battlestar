1473d1c59c063961f9b5b894c085cad831ad73f6
Dennis Horte <dennis@pinterest.com>
2022-11-03 21:44:41 -0700

    Vue: Upgrade vue.config.js to new API
    Not sure why I suddenly needed this.

--- app/vue.config.js
+++ app/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `@import "@/styles/_variables.scss";`
+        additionalData: `@import "@/styles/_variables.scss";`
       }
     }
   },
