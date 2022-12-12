28ac0cb3250ad7d83eb28f0ce29fa2d5d18ac20d
Dennis Horte <dennis@pinterest.com>
2022-12-12 13:37:35 -0800

    UI: Rematch Button automatically saves; only affects Magic

--- app/src/modules/games/common/components/RematchButton.vue
+++ app/src/modules/games/common/components/RematchButton.vue
@@ -9,10 +9,12 @@ import axios from 'axios'
 export default {
   name: 'RematchButton',
 
-  inject: ['game'],
+  inject: ['game', 'save'],
 
   methods: {
     async rematch() {
+      await save()
+
       const result = await axios.post('/api/game/rematch', { gameId: this.game._id })
       if (result.data.status === 'success') {
         this.$router.push(result.data.redirect)
