d82c5f0c7d7d4b49b70d72dc3cb206378e614ade
Dennis Horte <dennis@pinterest.com>
2023-05-07 10:04:31 -0700

    Cube Draft: A variety of pick orders

--- common/magic/draft/cube_draft.test.js
+++ common/magic/draft/cube_draft.test.js
@@ -16,21 +16,82 @@ describe('CubeDraft', () => {
     // If no errors thrown, success.
   })
 
-  test('dennis pick', () => {
-    const game = t.fixture()
+  describe('make picks', () => {
 
-    const request1 = game.run()
-    const request2 = t.choose(game, request1, 'dennis', 'agility')
+    test('dennis first pick', () => {
+      const game = t.fixture()
 
-    t.testBoard(game, {
-      dennis: {
-        picked: ['agility'],
-        waiting: [],
-      },
-      micah: {
-        picked: [],
-        waiting: ['micah-0', 'dennis-0'],
-      },
+      const request1 = game.run()
+      const request2 = t.choose(game, request1, 'dennis', 'agility')
+
+      t.testBoard(game, {
+        dennis: {
+          picked: ['agility'],
+          waiting: [],
+        },
+        micah: {
+          picked: [],
+          waiting: ['micah-0', 'dennis-0'],
+        },
+      })
+    })
+
+    test('micah first pick', () => {
+      const game = t.fixture()
+
+      const request1 = game.run()
+      const request2 = t.choose(game, request1, 'micah', 'lightning bolt')
+
+      t.testBoard(game, {
+        dennis: {
+          picked: [],
+          waiting: ['dennis-0', 'micah-0'],
+        },
+        micah: {
+          picked: ['lightning bolt'],
+          waiting: [],
+        },
+      })
+    })
+
+    test('dennis micah dennis', () => {
+      const game = t.fixture()
+
+      const request1 = game.run()
+      const request2 = t.choose(game, request1, 'dennis', 'agility')
+      const request3 = t.choose(game, request2, 'micah', 'lightning bolt')
+      const request4 = t.choose(game, request3, 'dennis', 'mountain')
+
+      t.testBoard(game, {
+        dennis: {
+          picked: ['agility', 'mountain'],
+          waiting: [],
+        },
+        micah: {
+          picked: ['lightning bolt'],
+          waiting: ['dennis-0', 'micah-0'],
+        },
+      })
+    })
+
+    test('dennis micah micah', () => {
+      const game = t.fixture()
+
+      const request1 = game.run()
+      const request2 = t.choose(game, request1, 'dennis', 'agility')
+      const request3 = t.choose(game, request2, 'micah', 'lightning bolt')
+      const request4 = t.choose(game, request3, 'micah', 'advance scout')
+
+      t.testBoard(game, {
+        dennis: {
+          picked: ['agility'],
+          waiting: ['micah-0', 'dennis-0'],
+        },
+        micah: {
+          picked: ['lightning bolt', 'advance scout'],
+          waiting: [],
+        },
+      })
     })
   })
 
