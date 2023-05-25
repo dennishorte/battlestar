54e88eced8ffee40f42bf4c4f62443da66c711e0
Dennis Horte <dennis@pinterest.com>
2023-05-24 21:18:20 -0700

    Cube Draft: Separate CardTableau from CardTableauModal

---
+++ app/src/modules/games/cube_draft/components/CardTableau.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card-tableau">
+    <div class="card-holder" v-for="card in cardData" :key="card.id">
+      <Card :size="220" :card="card" />
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { util } from 'battlestar-common'
+
+import Card from '@/modules/magic/components/Card'
+import Modal from '@/components/Modal'
+
+
+export default {
+  name: 'CardTableau',
+
+  components: {
+    Card,
+    Modal,
+  },
+
+  props: {
+    cards: {
+      type: Array,
+      default: [],
+    },
+  },
+
+  computed: {
+    cardData() {
+      if (this.cards.length === 0) {
+        return []
+      }
+      else if (this.cards[0].card_faces) {
+        return this.cards
+      }
+      else if (this.cards[0].data) {
+        return this.cards.map(c => c.data)
+      }
+      else if (this.cards[0].name) {
+        return this.cards.map(c => this.$store.getters['magic/cards/getLookupFunc'](c))
+      }
+    },
+  },
+}
+</script>
+
+
+<style scoped>
+.card-tableau {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.card-holder {
+  max-width: 100%;
+  overflow-x: scroll;
+  margin-left: .25em;
+  margin-bottom: .25em;
+}
+</style>
--- app/src/modules/games/cube_draft/components/CardTableauModal.vue
+++ app/src/modules/games/cube_draft/components/CardTableauModal.vue
@@ -2,9 +2,7 @@
   <Modal class="modal-xl">
     <div class="modal-body">
 
-      <div class="card-holder" v-for="card in cardData" :key="card.id">
-        <Card :size="220" :card="card" />
-      </div>
+      <CardTableau :cards="cards" />
 
     </div>
   </Modal>
@@ -14,15 +12,15 @@
 <script>
 import { util } from 'battlestar-common'
 
-import Card from '@/modules/magic/components/Card'
+import CardTableau from './CardTableau'
 import Modal from '@/components/Modal'
 
 
 export default {
-  name: 'CardCloseupModal',
+  name: 'CardTableauModal',
 
   components: {
-    Card,
+    CardTableau,
     Modal,
   },
 
@@ -32,39 +30,12 @@ export default {
       default: [],
     },
   },
-
-  computed: {
-    cardData() {
-      if (this.cards.length === 0) {
-        return []
-      }
-      else if (this.cards[0].card_faces) {
-        return this.cards
-      }
-      else if (this.cards[0].data) {
-        return this.cards.map(c => c.data)
-      }
-      else if (this.cards[0].name) {
-        return this.cards.map(c => this.$store.getters['magic/cards/getLookupFunc'](c))
-      }
-    },
-  },
 }
 </script>
 
 
 <style scoped>
-.card-holder {
-  max-width: 100%;
-  overflow-x: scroll;
-  margin-left: .25em;
-  margin-bottom: .25em;
-}
-
 .modal-body {
   width: 100%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
 }
 </style>
