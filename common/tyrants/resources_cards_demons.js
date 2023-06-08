84a362a4c7cdd233a15cff3d267b25f25570f3ff
Dennis Horte <dennis@pinterest.com>
2023-06-08 15:51:07 -0700

    Tyrants: Templates for demon and elemental cards

---
+++ common/tyrants/resources_cards_demons.js
@@ -0,0 +1,279 @@
+[
+  {
+    "name": "Ghoul",
+    "aspect": "malice",
+    "race": "undead",
+    "expansion": "Demons",
+    "cost": 4,
+    "points": 2,
+    "innerPoints": 4,
+    "count": 3,
+    "text": [
+      "+2 power",
+      "Each opponent recruits an Insane Outcast"
+    ]
+  },
+  {
+    "name": "Mind Flayer",
+    "aspect": "malice",
+    "race": "illithid",
+    "expansion": "Demons",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 3,
+    "text": [
+      "Devour a card in your hand > Choose one:",
+      "- +3 influence",
+      "- Assassinate a troop"
+    ]
+  },
+  {
+    "name": "Glavrezu",
+    "aspect": "malice",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 5,
+    "points": 2,
+    "innerPoints": 4,
+    "count": 1,
+    "text": [
+      "Devour a card in your hand > Assassinate 2 troops."
+    ]
+  },
+  {
+    "name": "Marilith",
+    "aspect": "malice",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 6,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 2,
+    "text": [
+      "Devour a card in your hand > +5 power"
+    ]
+  },
+  {
+    "name": "Orcus",
+    "aspect": "malice",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 8,
+    "points": 5,
+    "innerPoints": 1,
+    "count": 1,
+    "text": [
+      "Devour a card in your hand > Assassinate 2 troops. Then you may take up to 2 troops from any trophy halls and deploy them anywhere on the board."
+    ]
+  },
+  {
+    "name": "Gibbering Mouther",
+    "aspect": "conquest",
+    "race": "aberration",
+    "expansion": "Demons",
+    "cost": 2,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 3,
+    "text": [
+      "Deploy 2 troops, then choose an opponent with a troop adjacent to at least 1 of them. He or she recruits an Insane Outcast."
+    ]
+  },
+  {
+    "name": "Derro",
+    "aspect": "conquest",
+    "race": "derro",
+    "expansion": "Demons",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 4,
+    "count": 2,
+    "text": [
+      "Supplant a white troop anywhere on the board. You recruit an Insane Outcast."
+    ]
+  },
+  {
+    "name": "Ettin",
+    "aspect": "conquest",
+    "race": "giant",
+    "expansion": "Demons",
+    "cost": 4,
+    "points": 1,
+    "innerPoints": 2,
+    "count": 3,
+    "text": [
+      "Choose one:",
+      "- Deploy 3 troops.",
+      "- Assassinate 2 white troops."
+    ]
+  },
+  {
+    "name": "Balor",
+    "aspect": "conquest",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 6,
+    "points": 3,
+    "innerPoints": 6,
+    "count": 1,
+    "text": [
+      "Devour a card in your hand > Supplant a white troop anywhere on the board. Then deploy a white troop."
+    ]
+  },
+  {
+    "name": "Demogorgon",
+    "aspect": "conquest",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 8,
+    "points": 5,
+    "innerPoints": 1,
+    "count": 1,
+    "text": [
+      "Devour a card in your hand > Supplant 2 white troops. Each opponent recruits 2 Insane Outcasts."
+    ]
+  },
+  {
+    "name": "Night Hag",
+    "aspect": "guile",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 3,
+    "text": [
+      "Choose one:",
+      "- Place a spy",
+      "- Return one of your spies > Draw 2 cards."
+    ]
+  },
+  {
+    "name": "Jackalwere",
+    "aspect": "guile",
+    "race": "shapechanger",
+    "expansion": "Demons",
+    "cost": 4,
+    "points": 2,
+    "innerPoints": 4,
+    "count": 3,
+    "text": [
+      "Choose one:",
+      "- Place a spy",
+      "- Return one of your spies > +2 power, +2 influence"
+    ]
+  },
+  {
+    "name": "Succubus",
+    "aspect": "guile",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 5,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 2,
+    "text": [
+      "Devour a card in your hand > Place a spy, then assassinate a troop at that spy’s site."
+    ]
+  },
+  {
+    "name": "Vrock",
+    "aspect": "guile",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 5,
+    "points": 2,
+    "innerPoints": 4,
+    "count": 1,
+    "text": [
+      "Choose one:",
+      "- Place a spy.",
+      "- Return one of your spies > +5 power"
+    ]
+  },
+  {
+    "name": "Graz’zt",
+    "aspect": "guile",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 6,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 1,
+    "text": [
+      "Choose one:",
+      "- Place 2 spies.",
+      "- Return any number of your spies > Supplant a troop at each of the returned spies’ sites."
+    ]
+  },
+  {
+    "name": "Myconid Adult",
+    "aspect": "ambition",
+    "race": "myconid",
+    "expansion": "Demons",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 3,
+    "text": [
+      "+2 influence",
+      "Choose an opponent. He or she recruits an Insane Outcast."
+    ]
+  },
+  {
+    "name": "Nalfeshnee",
+    "aspect": "ambition",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 5,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 2,
+    "text": [
+      "+3 influence",
+      "Promote the top card of your deck."
+    ]
+  },
+  {
+    "name": "Hezrou",
+    "aspect": "ambition",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 2,
+    "text": [
+      "Move an enemy troop.",
+      "Promote the top card of your deck."
+    ]
+  },
+  {
+    "name": "Myconid Sovereign",
+    "aspect": "ambition",
+    "race": "myconid",
+    "expansion": "Demons",
+    "cost": 4,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 2,
+    "text": [
+      "Choose an opponent. He or she recruits an Insane Outcast.",
+      "At end of turn, promote another card played this turn."
+    ]
+  },
+  {
+    "name": "Zuggtmoy",
+    "aspect": "ambition",
+    "race": "fiend",
+    "expansion": "Demons",
+    "cost": 6,
+    "points": 3,
+    "innerPoints": 6,
+    "count": 1,
+    "text": [
+      "Devour a card in your inner circle > +3 influence and at end of turn, promote up to 2 other cards played this turn."
+    ]
+  }
+]
---
+++ common/tyrants/resources_cards_elementals.js
@@ -0,0 +1,291 @@
+[
+  {
+    "name": "Eternal Flame Cultist",
+    "aspect": "malice",
+    "race": "human",
+    "expansion": "Elementals",
+    "cost": 4,
+    "points": 2,
+    "innerPoints": 4,
+    "count": 3,
+    "text": [
+      "Assassinate a troop",
+      "Malice Focus > +2 power"
+    ]
+  },
+  {
+    "name": "Fire Elemental",
+    "aspect": "malice",
+    "race": "elemental",
+    "expansion": "Elementals",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 3,
+    "text": [
+      "Choose one:",
+      "- +2 power",
+      "- +2 influence",
+      "Malice focus > Draw a card"
+    ]
+  },
+  {
+    "name": "Fire Elemental Myrmidon",
+    "aspect": "malice",
+    "race": "elemental",
+    "expansion": "Elementals",
+    "cost": 4,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 2,
+    "text": [
+      "+2 power",
+      "At end of turn, promote an Obedience card played this turn"
+    ]
+  },
+  {
+    "name": "Vanifer",
+    "aspect": "malice",
+    "race": "tiefling",
+    "expansion": "Elementals",
+    "cost": 5,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 1,
+    "text": [
+      "Assassinate a troop",
+      "Recruit a Malice card that costs 4 or less without paying its cost"
+    ]
+  },
+  {
+    "name": "Imix",
+    "aspect": "malice",
+    "race": "elemental prince",
+    "expansion": "Elementals",
+    "cost": 6,
+    "points": 3,
+    "innerPoints": 6,
+    "count": 1,
+    "text": [
+      "+4 power",
+      "Malice Focus > +2 power"
+    ]
+  },
+  {
+    "name": "Crushing Wave Cultist",
+    "aspect": "conquest",
+    "race": "human",
+    "expansion": "Elementals",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 4,
+    "count": 3,
+    "text": [
+      "Assassinate a white troop",
+      "Conquest Focus > Deploy 2 troops"
+    ]
+  },
+  {
+    "name": "Water Elemental",
+    "aspect": "conquest",
+    "race": "elemental",
+    "expansion": "Elementals",
+    "cost": 2,
+    "points": 1,
+    "innerPoints": 2,
+    "count": 3,
+    "text": [
+      "Deploy 2 troops",
+      "Conquest Focus > Draw a card"
+    ]
+  },
+  {
+    "name": "Water Elemental Myrmidon",
+    "aspect": "conquest",
+    "race": "elemental",
+    "expansion": "Elementals",
+    "cost": 4,
+    "points": 2,
+    "innerPoints": 4,
+    "count": 2,
+    "text": [
+      "Assassinate a white troop",
+      "At end of turn, promote an Obedience card played this turn"
+    ]
+  },
+  {
+    "name": "Gar Shatterkeel",
+    "aspect": "conquest",
+    "race": "human",
+    "expansion": "Elementals",
+    "cost": 5,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 1,
+    "text": [
+      "Deploy 3 troops",
+      "Recruit a Conquest card that costs 4 or less without paying its cost"
+    ]
+  },
+  {
+    "name": "Olhydra",
+    "aspect": "conquest",
+    "race": "elemental prince",
+    "expansion": "Elementals",
+    "cost": 6,
+    "points": 3,
+    "innerPoints": 6,
+    "count": 1,
+    "text": [
+      "Supplant a white troop anywhere on the board.",
+      "Conquest Focus > Deploy 2 troops."
+    ]
+  },
+  {
+    "name": "Air Elemental",
+    "aspect": "guile",
+    "race": "elemental",
+    "expansion": "Elementals",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 2,
+    "count": 4,
+    "text": [
+      "Choose one:",
+      "- Place a spy",
+      "- Return one of your spies > Deploy 3 troops",
+      "Guile Focus > Draw a card"
+    ]
+  },
+  {
+    "name": "Howling Hatred Cultist",
+    "aspect": "guile",
+    "race": "human",
+    "expansion": "Elementals",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 2,
+    "text": [
+      "Choose one:",
+      "- Place a spy",
+      "- Return one of your spies > +3 influence",
+      "Guile Focus > +1 power"
+    ]
+  },
+  {
+    "name": "Air Elemental Myrmidon",
+    "aspect": "guile",
+    "race": "elemental",
+    "expansion": "Elementals",
+    "cost": 4,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 2,
+    "text": [
+      "Place a spy.",
+      "At end of turn, promote an Obedience card played this turn."
+    ]
+  },
+  {
+    "name": "Aerisi Kalinoth",
+    "aspect": "guile",
+    "race": "elf",
+    "expansion": "Elementals",
+    "cost": 5,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 1,
+    "text": [
+      "+1 power",
+      "Place a spy.",
+      "Recruit a Guile card that costs 4 or less without paying its cost."
+    ]
+  },
+  {
+    "name": "Yan-C-Bin",
+    "aspect": "guile",
+    "race": "elemental prince",
+    "expansion": "Elementals",
+    "cost": 6,
+    "points": 3,
+    "innerPoints": 6,
+    "count": 1,
+    "text": [
+      "Place a spy, then assassinate a troop at that spy’s site.",
+      "Guile Focus > Place a spy"
+    ]
+  },
+  {
+    "name": "Black Earth Cultist",
+    "aspect": "ambition",
+    "race": "human",
+    "expansion": "Elementals",
+    "cost": 2,
+    "points": 1,
+    "innerPoints": 2,
+    "count": 4,
+    "text": [
+      "At end of turn, promote another card played this turn.",
+      "Ambition Focus > +2 influence"
+    ]
+  },
+  {
+    "name": "Earth Elemental",
+    "aspect": "ambition",
+    "race": "elemental",
+    "expansion": "Elementals",
+    "cost": 3,
+    "points": 1,
+    "innerPoints": 3,
+    "count": 2,
+    "text": [
+      "+1 influence",
+      "Return another player’s troop or spy.",
+      "Ambition focus > Draw a card"
+    ]
+  },
+  {
+    "name": "Earth Elemental Myrmidon",
+    "aspect": "ambition",
+    "race": "elemental",
+    "expansion": "Elementals",
+    "cost": 4,
+    "points": 2,
+    "innerPoints": 4,
+    "count": 2,
+    "text": [
+      "+2 influence",
+      "At end of turn, promote another card played this turn"
+    ]
+  },
+  {
+    "name": "Marlos Urnrayle",
+    "aspect": "ambition",
+    "race": "medusa",
+    "expansion": "Elementals",
+    "cost": 5,
+    "points": 2,
+    "innerPoints": 5,
+    "count": 1,
+    "text": [
+      "At end of turn, promote another card played this turn.",
+      "Recruit an Ambition card that costs 4 or less without paying its cost."
+    ]
+  },
+  {
+    "name": "Ogremoch",
+    "aspect": "ambition",
+    "race": "elemental prince",
+    "expansion": "Elementals",
+    "cost": 6,
+    "points": 3,
+    "innerPoints": 7,
+    "count": 1,
+    "text": [
+      "+2 influence",
+      "At end of turn, promote another card played this turn.",
+      "Ambition Focus > At end of turn, promote another card played this turn."
+    ]
+  }
+]
