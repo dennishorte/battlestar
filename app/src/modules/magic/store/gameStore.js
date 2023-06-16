ebac418312bb30306bc6404cc97ef50595c1ef04
Dennis Horte <dennis@pinterest.com>
2023-06-16 11:47:18 -0700

    Magic bugfix: After doing moveall, clicking on any card caused a crash

--- app/src/modules/magic/store/gameStore.js
+++ app/src/modules/magic/store/gameStore.js
@@ -173,6 +173,7 @@ export default {
         sourceId: state.movingAllSource,
         targetId: targetZone.id,
       })
+      commit('setMovingAllSource', null)
     },
 
     moveRevealed({ commit, state }, targetZone) {
