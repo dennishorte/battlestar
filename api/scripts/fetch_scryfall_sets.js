afe21653ffd7f2aecb70c37776409e3e9085ca11
Dennis Horte <dennis@pinterest.com>
2023-06-23 16:43:01 -0700

    Magic: Script for fetching set data from Scryfall

---
+++ api/scripts/fetch_scryfall_sets.js
@@ -0,0 +1,48 @@
+const axios = require('axios')
+const fs = require('fs')
+
+
+async function fetchScryfallSets(uri) {
+  const result = await axios.get(uri, {
+    maxBodyLength: Infinity,
+    maxContentLength: Infinity,
+    timeout: 0,
+  })
+
+  if (result.status !== 200) {
+    return {
+      status: 'error',
+      message: result.statusText,
+    }
+  }
+
+  return result.data.data
+}
+
+async function sortData(data) {
+  data.sort((l, r) => r.released_at < l.released_at)
+}
+
+async function fetchFromScryfallAndClean() {
+  console.log('Fetching latest SET data from scryfall')
+
+  const downloadUri = 'https://api.scryfall.com/sets'
+  const outputFilename = 'set_data/sets.raw'
+
+  console.log('...downloading set data from ' + downloadUri)
+  const sets = await fetchScryfallSets(downloadUri)
+
+  console.log('...cleaning data')
+  sortData(sets)
+
+  console.log('...writing raw data to disk: ' + outputFilename)
+  fs.writeFileSync(outputFilename, JSON.stringify(sets, null, 2))
+
+  console.log('...done')
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Main
+
+fetchFromScryfallAndClean()
