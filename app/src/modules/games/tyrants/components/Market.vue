45acbb937be066d12c6766bd210db449679fe544
Dennis Horte <dennis@pinterest.com>
2023-09-04 18:51:43 -0700

    Tyrants: Add counts beside the house guard and priestess piles

--- app/src/modules/games/tyrants/components/Market.vue
+++ app/src/modules/games/tyrants/components/Market.vue
@@ -4,8 +4,17 @@
       <div class="title">Market</div>
       <div class="cards-remaining">({{ cardsRemaining }})</div>
     </div>
-    <GameCard v-if="Boolean(guard)" :card="guard" :show-cost="true" />
-    <GameCard v-if="Boolean(priestess)" :card="priestess" :show-cost="true" />
+
+    <div v-if="Boolean(guard)" class="card-with-count">
+      <GameCard :card="guard" :show-cost="true" class="card-by-count" />
+      <div class="card-count">{{ game.getZoneById('guard').cards().length }}</div>
+    </div>
+
+    <div v-if="Boolean(priestess)" class="card-with-count">
+      <GameCard :card="priestess" :show-cost="true" class="card-by-count" />
+      <div class="card-count">{{ game.getZoneById('priestess').cards().length }}</div>
+    </div>
+
     <hr class="market-separator" />
     <GameCard v-for="card in cards" :key="card.id" :card="card" :show-cost="true" />
   </div>
@@ -55,6 +64,25 @@ export default {
   margin: 0 -15px;
   margin-bottom: 1em;
   padding: 5px 15px;
+}
+
+.card-with-count {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.card-by-count {
+  flex-grow: 2;
+  margin-right: .25em;
+}
+
+.card-count {
+  color: #eee1f2;
+  background-color: #78498c;
+  padding: 4px;
+  border: 1px solid #3b2345;
+  border-radius: 3px;
 }
 
 .cards-remaining {
