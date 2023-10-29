f67d816149b83f1484ac3e3e796c52fc670dd959
Dennis Horte <dennis@pinterest.com>
2023-10-29 12:28:23 -0700

    Tyrants: Add color to troop and spy names in the log

--- app/src/modules/games/tyrants/components/log/CardName.vue
+++ app/src/modules/games/tyrants/components/log/CardName.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card-name" @click="clicked">
+  <div class="card-name" @click="clicked" :class="classes" :style="styles">
     {{ displayName }}
   </div>
 </template>
@@ -20,8 +20,36 @@ export default {
       return this.game.getCardById(this.name)
     },
 
+    classes() {
+      const classes = []
+
+      if (!this.game.settings.chooseColors && this.tokenOwner) {
+        const color = this.ui.fn.getPlayerColor(this.game, this.tokenOwner)
+        classes.push(`${color}-element`)
+      }
+
+      return classes
+    },
+
     displayName() {
       return this.card ? this.card.name : this.name
+    },
+
+    styles() {
+      const output = {}
+
+      if (this.game.settings.chooseColors && this.tokenOwner) {
+        output['background-color'] = this.tokenOwner.color
+      }
+
+      return output
+    },
+
+    tokenOwner() {
+      const nameBits = this.name.split('-')
+      if (nameBits.length > 1 && (nameBits[0] === 'troop' || nameBits[0] === 'spy')) {
+        return this.game.getPlayerByName(nameBits[1])
+      }
     },
   },
 
