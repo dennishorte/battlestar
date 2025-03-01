581c9277e78f5be6cfc806317c2f3ae82a4ee81c
dennis <dennis.horte@gmail.com>
2025-02-28 19:06:49 -0800

    Handshake

--- common/ultimate/res/usee/Handshake.js
+++ common/ultimate/res/usee/Handshake.js
@@ -1,11 +1,12 @@
 const CardBase = require(`../CardBase.js`)
+const util = require('../../../lib/util.js')
 
 function Card() {
   this.id = `Handshake`  // Card names are unique in Innovation
   this.name = `Handshake`
   this.color = `yellow`
   this.age = 1
-  this.expansion = `uce`  
+  this.expansion = `uce`
   this.biscuits = `hckk`
   this.dogmaBiscuit = `c`
   this.inspire = ``
@@ -20,15 +21,19 @@ function Card() {
       // Transfer all cards from leader's hand to player's hand
       const leaderHand = game.getZoneByPlayer(leader, 'hand')
       const leaderCards = leaderHand.cards()
-      game.aTransferMany(leader, leaderCards, game.getZoneByPlayer(player, 'hand'))
-      
-      // Have player choose two colors
-      const chosenColors = game.aChooseColors(player, 2)
+      game.aTransferMany(leader, leaderCards, game.getZoneByPlayer(player, 'hand'), { ordered: true })
 
-      // Transfer all cards of chosen colors from player's hand to leader's hand  
+      // Have player choose two colors
+      const handColors = game
+        .getCardsByZone(player, 'hand')
+        .map(c => c.color)
+      const uniqueColors = util.array.distinct(handColors).sort()
+      const chosenColors = game.aChoose(player, uniqueColors, { count: 2 })
+
+      // Transfer all cards of chosen colors from player's hand to leader's hand
       const playerHand = game.getZoneByPlayer(player, 'hand')
       const transferCards = playerHand.cards().filter(card => chosenColors.includes(card.color))
-      game.aTransferMany(player, transferCards, leaderHand)
+      game.aTransferMany(player, transferCards, leaderHand, { ordered: true })
     },
   ]
   this.echoImpl = []
--- common/ultimate/res/usee/Handshake.test.js
+++ common/ultimate/res/usee/Handshake.test.js
@@ -7,17 +7,26 @@ describe('Handshake', () => {
     t.setBoard(game, {
       dennis: {
         yellow: ['Handshake'],
+        hand: ['Domestication', 'Masonry', 'Writing'],
+      },
+      micah: {
+        hand: ['The Wheel', 'Tools']
       },
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Handshake')
+    request = t.choose(game, request, 'blue', 'green')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
         yellow: ['Handshake'],
+        hand: ['Writing', 'Tools', 'The Wheel'],
+      },
+      micah: {
+        hand: ['Domestication', 'Masonry'],
       },
     })
   })
