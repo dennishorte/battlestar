83f69fb609223df29352d68e0dc049fbebae649e
dennis <dennis.horte@gmail.com>
2025-03-03 08:37:36 -0800

    Cipher

--- common/ultimate/res/usee/Cipher.js
+++ common/ultimate/res/usee/Cipher.js
@@ -7,13 +7,13 @@ function Card() {
   this.age = 2
   this.expansion = `usee`
   this.biscuits = `hssk`
-  this.dogmaBiscuit = `p`
+  this.dogmaBiscuit = `s`
   this.inspire = ``
   this.echo = ``
   this.karma = []
   this.dogma = [
     `Return all cards from your hand. If you return at least two, draw a card of value one higher than the highest value of card you return.`,
-    `Draw a [3]. You may splay your blue cards left.`
+    `Draw a {2}. You may splay your blue cards left.`
   ]
 
   this.dogmaImpl = [
@@ -21,12 +21,12 @@ function Card() {
       const returned = game.aReturnMany(player, game.getCardsByZone(player, 'hand'))
 
       if (returned.length >= 2) {
-        const highestValue = Math.max(...returned.map(card => card.age))
+        const highestValue = Math.max(...returned.map(card => card.getAge()))
         game.aDraw(player, { age: highestValue + 1 })
       }
     },
     (game, player) => {
-      game.aDraw(player, { age: 3 })
+      game.aDraw(player, { age: game.getEffectAge(this, 2) })
       game.aChooseAndSplay(player, ['blue'], 'left')
     }
   ]
--- common/ultimate/res/usee/Cipher.test.js
+++ common/ultimate/res/usee/Cipher.test.js
@@ -2,12 +2,18 @@ Error.stackTraceLimit = 100
 const t = require('../../testutil.js')
 describe('Cipher', () => {
 
-  test('dogma', () => {
+  test('dogma: one card in hand', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
     t.setBoard(game, {
       dennis: {
         green: ['Cipher'],
+        hand: ['Software'],
       },
+      decks: {
+        usee: {
+          2: ['Password'],
+        },
+      }
     })
 
     let request
@@ -18,6 +24,44 @@ describe('Cipher', () => {
     t.testBoard(game, {
       dennis: {
         green: ['Cipher'],
+        hand: ['Password'],
+      },
+    })
+  })
+
+  test('dogma: two cards in hand', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        green: ['Cipher'],
+        blue: ['Mathematics', 'Publications'],
+        hand: ['Software', 'Tools'],
+      },
+      decks: {
+        base: {
+          2: ['Construction'],
+        },
+        usee: {
+          11: ['Cloaking'],
+        },
+      }
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Cipher')
+    request = t.choose(game, request, 'auto')
+    request = t.choose(game, request, 'blue')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        green: ['Cipher'],
+        blue: {
+          cards: ['Mathematics', 'Publications'],
+          splay: 'left',
+        },
+        hand: ['Construction', 'Cloaking']
       },
     })
   })
