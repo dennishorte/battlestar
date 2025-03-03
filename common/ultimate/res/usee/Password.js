d7b6c92578577d8c3ad55f9ee8dfbf7a029d0156
dennis <dennis.horte@gmail.com>
2025-03-03 08:07:38 -0800

    Password

--- common/ultimate/res/usee/Password.js
+++ common/ultimate/res/usee/Password.js
@@ -7,42 +7,38 @@ function Card() {
   this.age = 2
   this.expansion = `base` // verified against image
   this.biscuits = `hckk`
-  this.dogmaBiscuit = `p`
+  this.dogmaBiscuit = `k`
   this.inspire = ``
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `Draw and reveal a [2]. You may safeguard another card from your hand of the color of the drawn card. If you do, score the drawn card. Otherwise, return all cards from your hand except the drawn card.`
+    `Draw and reveal a {2}. You may safeguard another card from your hand of the color of the drawn card. If you do, score the drawn card. Otherwise, return all cards from your hand except the drawn card.`
   ]
 
   this.dogmaImpl = [
     (game, player) => {
       const drawnCard = game.aDrawAndReveal(player, game.getEffectAge(this, 2))
-      
-      const handCards = game.getZoneByPlayer(player, 'hand').cards()
-      const sameColorCards = handCards.filter(c => c.color === drawnCard.color)
-      
-      if (sameColorCards.length > 0) {
-        const safeguarded = game.aChooseCard(player, sameColorCards, {
-          title: 'Choose a card to safeguard',
-          min: 0,
-          max: 1
+
+      const otherHandCards = game
+        .getCardsByZone(player, 'hand')
+        .filter(c => c.name !== drawnCard.name)
+      const sameColorCards = otherHandCards.filter(c => c.color === drawnCard.color)
+
+      const safeguarded = game.aChooseAndSafeguard(player, sameColorCards, {
+        title: 'Choose a card to safeguard',
+        min: 0,
+        max: 1
+      })[0]
+
+      if (safeguarded) {
+        game.mLog({
+          template: '{player} safeguards {card}',
+          args: { player, card: safeguarded }
         })
-        
-        if (safeguarded) {
-          game.mLog({
-            template: '{player} safeguards {card}',
-            args: { player, card: safeguarded }
-          })
-          game.aScore(player, drawnCard)
-        } else {
-          const toReturn = handCards.filter(c => c.id !== drawnCard.id)
-          game.aReturnMany(player, toReturn)
-        }
-      } else {
-        // No cards of same color, return all but drawn card
-        const toReturn = handCards.filter(c => c.id !== drawnCard.id) 
-        game.aReturnMany(player, toReturn)
+        game.aScore(player, drawnCard)
+      }
+      else {
+        game.aReturnMany(player, otherHandCards)
       }
     },
   ]
--- common/ultimate/res/usee/Password.test.js
+++ common/ultimate/res/usee/Password.test.js
@@ -2,22 +2,59 @@ Error.stackTraceLimit = 100
 const t = require('../../testutil.js')
 describe('Password', () => {
 
-  test('dogma', () => {
+  test('dogma: safeguard', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
     t.setBoard(game, {
       dennis: {
         red: ['Password'],
+        hand: ['Metalworking'],
       },
+      decks: {
+        usee: {
+          2: ['Padlock'],
+        }
+      }
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Password')
+    request = t.choose(game, request, 'Metalworking')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
         red: ['Password'],
+        score: ['Padlock'],
+        safe: ['Metalworking'],
+      },
+    })
+  })
+
+  test('dogma: no safeguard', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        red: ['Password'],
+        hand: ['Metalworking'],
+      },
+      decks: {
+        usee: {
+          2: ['Padlock'],
+        }
+      }
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Password')
+    request = t.choose(game, request, )
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Password'],
+        hand: ['Padlock'],
       },
     })
   })
