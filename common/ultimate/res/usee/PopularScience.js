4a3ef1982ecd941a130ce654eae6264e6b6cdcd3
dennis <dennis.horte@gmail.com>
2025-03-06 15:47:41 -0800

    Popular Science

--- common/ultimate/res/usee/PopularScience.js
+++ common/ultimate/res/usee/PopularScience.js
@@ -1,4 +1,5 @@
 const CardBase = require(`../CardBase.js`)
+const util = require('../../../lib/util.js')
 
 function Card() {
   this.id = `Popular Science`  // Card names are unique in Innovation
@@ -6,7 +7,7 @@ function Card() {
   this.color = `blue`
   this.age = 5
   this.expansion = `usee`
-  this.biscuits = `scsh` 
+  this.biscuits = `scsh`
   this.dogmaBiscuit = `s`
   this.inspire = ``
   this.echo = ``
@@ -21,32 +22,31 @@ function Card() {
     (game, player) => {
       const topGreenCards = game
         .getPlayerAll()
-        .flatMap(player => game.getTopCards(player))
-        .filter(card => card.color === 'green')
-      
+        .map(player => game.getTopCard(player, 'green'))
+
       if (topGreenCards.length === 0) {
-        game.mLogNoEffect()
+        game.mLog({ template: 'No player has a top green card.' })
         return
       }
 
-      const ages = topGreenCards.map(card => card.age)
+      const ages = util.array.distinct(topGreenCards.map(card => card.getAge()))
       const age = game.aChooseAge(player, ages, { title: 'Choose age of card to draw and meld' })
       game.aDrawAndMeld(player, age)
     },
 
     (game, player) => {
-      const topYellowCard = game.getTopCard(player, 'yellow') 
+      const topYellowCard = game.getTopCard(player, 'yellow')
       if (!topYellowCard) {
         game.mLogNoEffect()
         return
       }
 
-      const age = topYellowCard.age + 1
+      const age = topYellowCard.getAge() + 1
       game.aDrawAndMeld(player, age)
     },
 
     (game, player) => {
-      game.aChooseAndSplay(player, ['blue'], 'right') 
+      game.aChooseAndSplay(player, ['blue'], 'right')
     },
   ]
   this.echoImpl = []
--- common/ultimate/res/usee/PopularScience.test.js
+++ common/ultimate/res/usee/PopularScience.test.js
@@ -6,18 +6,42 @@ describe('Popular Science', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
     t.setBoard(game, {
       dennis: {
-        blue: ['Popular Science'],
+        yellow: ['Statistics'],
+        green: ['Paper'],
+        blue: ['Popular Science', 'Tools'],
+      },
+      micah: {
+        green: ['Navigation'],
+      },
+      decks: {
+        base: {
+          6: ['Canning'],
+        },
+        usee: {
+          4: ['Legend'],
+        }
       },
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Popular Science')
+    request = t.choose(game, request, '4')
+    request = t.choose(game, request, 'blue')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
-        blue: ['Popular Science'],
+        yellow: ['Canning', 'Statistics'],
+        green: ['Paper'],
+        blue: {
+          cards: ['Popular Science', 'Tools'],
+          splay: 'right',
+        },
+        purple: ['Legend'],
+      },
+      micah: {
+        green: ['Navigation'],
       },
     })
   })
