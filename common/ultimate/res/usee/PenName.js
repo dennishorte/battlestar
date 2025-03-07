39b82ba93197173855fafe606dfd76e093aa0e19
dennis <dennis.horte@gmail.com>
2025-03-07 08:37:01 -0800

    Pen Name

--- common/ultimate/res/usee/PenName.js
+++ common/ultimate/res/usee/PenName.js
@@ -17,36 +17,47 @@ function Card() {
 
   this.dogmaImpl = [
     (game, player) => {
-      const choices = ['Splay left', 'Meld and splay right']
-      const choice = game.aChoose(player, choices, { title: 'Choose one:' })
+      const choices = []
 
-      if (choice === choices[0]) {
-        // Splay left and self-execute
-        const unsplayedColors = ['red', 'green', 'yellow', 'blue'].filter(color => 
-          color !== 'purple' && game.getZoneByPlayer(player, color).splay === null
-        )
-        const color = game.aChooseColor(player, unsplayedColors)
-        if (color) {
-          game.aSplay(player, color, 'left')
-          const topCard = game.getTopCard(player, color)
-          if (topCard) {
-            game.mLog({
-              template: '{player} self-executes {card}',
-              args: { player, card: topCard }
-            })
-            game.aCardEffects(player, topCard, 'dogma', { leader: player })
-          }
+      const unsplayed = game
+        .utilColors()
+        .filter(c => c !== 'purple')
+        .map(c => game.getZoneByPlayer(player, c))
+        .filter(z => z.splay === 'none')
+        .map(z => z.color)
+
+      if (unsplayed.length > 0) {
+        choices.push({
+          title: 'Splay left and self-execute',
+          choices: unsplayed,
+          min: 0,
+        })
+      }
+
+      const handCards = game.getCardsByZone(player, 'hand')
+
+      if (handCards.length > 0) {
+        choices.push({
+          title: 'Meld and splay right',
+          choices: handCards.map(c => c.name),
+          min: 0,
+        })
+      }
+
+      const selected = game.aChoose(player, choices, { title: 'Choose one:' })[0]
+
+      if (selected.title === 'Splay left and self-execute') {
+        const color = selected.selection[0]
+        game.aSplay(player, color, 'left')
+        const topCard = game.getTopCard(player, color)
+        if (topCard) {
+          game.aSelfExecute(player, topCard)
         }
       }
-      else if (choice === choices[1]) {
-        // Meld from hand and splay right
-        const card = game.aChooseCard(player, game.getCardsByZone(player, 'hand'), {
-          title: 'Choose a card to meld'
-        })
-        if (card) {
-          game.aMeld(player, card)
-          game.aSplay(player, card.color, 'right')
-        }
+      else if (selected.title === 'Meld and splay right') {
+        const card = game.getCardByName(selected.selection[0])
+        game.aMeld(player, card)
+        game.aSplay(player, card.color, 'right')
       }
     },
   ]
