7b959cee8c670082cb63bfae75dcf3f7c9cba439
dennis <dennis.horte@gmail.com>
2025-03-07 09:19:43 -0800

    Hiking

--- common/ultimate/res/usee/Hiking.js
+++ common/ultimate/res/usee/Hiking.js
@@ -7,26 +7,26 @@ function Card() {
   this.age = 6
   this.expansion = `usee`
   this.biscuits = `llhs`
-  this.dogmaBiscuit = `l` 
+  this.dogmaBiscuit = `l`
   this.inspire = ``
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `Draw and reveal a {6}. If the top card on your board of the drawn card's color has {l}, tuck the drawn card and draw and reveal a {5}. If the second drawn card has {l}, meld it and draw an {8}.`
+    `Draw and reveal a {6}. If the top card on your board of the drawn card's color has {f}, tuck the drawn card and draw and reveal a {7}. If the second drawn card has {l}, meld it and draw an {8}.`
   ]
 
   this.dogmaImpl = [
     (game, player) => {
       const firstCard = game.aDrawAndReveal(player, game.getEffectAge(this, 6))
-      
+
       if (firstCard) {
         const topCard = game.getTopCard(player, firstCard.color)
 
-        if (topCard && topCard.checkHasBiscuit('l')) {
+        if (topCard && topCard.checkHasBiscuit('f')) {
           game.aTuck(player, firstCard)
-          
-          const secondCard = game.aDrawAndReveal(player, game.getEffectAge(this, 5))
-          
+
+          const secondCard = game.aDrawAndReveal(player, game.getEffectAge(this, 7))
+
           if (secondCard && secondCard.checkHasBiscuit('l')) {
             game.aMeld(player, secondCard)
             game.aDraw(player, { age: game.getEffectAge(this, 8) })
--- common/ultimate/res/usee/Hiking.test.js
+++ common/ultimate/res/usee/Hiking.test.js
@@ -6,7 +6,44 @@ describe('Hiking', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
     t.setBoard(game, {
       dennis: {
+        red: ['Optics'],
         green: ['Hiking'],
+      },
+      decks: {
+        usee: {
+          6: ['Sniping'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Hiking')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Optics'],
+        green: ['Hiking'],
+        hand: ['Sniping'],
+      },
+    })
+  })
+
+  test('dogma: has f', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        green: ['Hiking'],
+        red: ['Gunpowder'],
+      },
+      decks: {
+        base: {
+          7: ['Railroad'],
+        },
+        usee: {
+          6: ['Sniping'],
+        },
       },
     })
 
@@ -18,6 +55,41 @@ describe('Hiking', () => {
     t.testBoard(game, {
       dennis: {
         green: ['Hiking'],
+        red: ['Gunpowder', 'Sniping'],
+        hand: ['Railroad'],
+      },
+    })
+  })
+
+  test('dogma: has f', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        green: ['Hiking'],
+        red: ['Gunpowder'],
+      },
+      decks: {
+        base: {
+          7: ['Lighting'],
+          8: ['Flight'],
+        },
+        usee: {
+          6: ['Sniping'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Hiking')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        green: ['Hiking'],
+        red: ['Gunpowder', 'Sniping'],
+        purple: ['Lighting'],
+        hand: ['Flight'],
       },
     })
   })
