88b77695d148c10aecab41b8b90ae8c03c4a3ef0
dennis <dennis.horte@gmail.com>
2025-03-09 08:57:51 -0700

    Subway

--- common/ultimate/res/usee/Subway.js
+++ common/ultimate/res/usee/Subway.js
@@ -12,26 +12,20 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `Draw and tuck a {7}. If you have at least seven visible cards on your board of the color of the tucked card, draw a {7}. Otherwise, junk all cards on your board of that color, and draw an {8}.`
+    `Draw and tuck a {7}. If you have at least seven visible cards on your board of the color of the tucked card, draw a {9}. Otherwise, junk all cards on your board of that color, and draw an {8}.`
   ]
 
   this.dogmaImpl = [
     (game, player) => {
-      const card = game.aDraw(player, { age: game.getEffectAge(this, 7) })
-      game.mTuck(player, card)
+      const card = game.aDrawAndTuck(player, game.getEffectAge(this, 7))
 
-      const color = card.color
-      const visibleCards = game
-        .getTopCards(player)
-        .filter(c => c.color === color)
-      
-      if (visibleCards.length >= 7) {
-        game.aDraw(player, { age: game.getEffectAge(this, 7) })
-      } else {
-        const toJunk = game
-          .getBoard(player)[color].cards()
+      const visibleCards = game.getVisibleCardsByZone(player, card.color)
 
-        game.aJunkMany(player, toJunk)
+      if (visibleCards >= 7) {
+        game.aDraw(player, { age: game.getEffectAge(this, 9) })
+      }
+      else {
+        game.aJunkMany(player, game.getCardsByZone(player, card.color), { ordered: true })
         game.aDraw(player, { age: game.getEffectAge(this, 8) })
       }
     },
--- common/ultimate/res/usee/Subway.test.js
+++ common/ultimate/res/usee/Subway.test.js
@@ -2,11 +2,22 @@ Error.stackTraceLimit = 100
 const t = require('../../testutil.js')
 describe('Subway', () => {
 
-  test('dogma', () => {
+  test('dogma: at least 7', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
     t.setBoard(game, {
       dennis: {
-        yellow: ['Subway'],
+        yellow: {
+          cards: ['Subway', 'Canning', 'Masonry', 'Fermenting', 'Machinery', 'Agriculture'],
+          splay: 'left',
+        }
+      },
+      decks: {
+        base: {
+          9: ['Computers'],
+        },
+        usee: {
+          7: ['Mafia'],
+        },
       },
     })
 
@@ -17,7 +28,70 @@ describe('Subway', () => {
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
-        yellow: ['Subway'],
+        yellow: {
+          cards: ['Subway', 'Canning', 'Masonry', 'Fermenting', 'Machinery', 'Agriculture', 'Mafia'],
+          splay: 'left',
+        },
+        hand: ['Computers'],
+      },
+    })
+  })
+
+  test('dogma: at least 7 (but not visible)', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        yellow: ['Subway', 'Canning', 'Masonry', 'Fermenting', 'Machinery', 'Agriculture'],
+      },
+      decks: {
+        base: {
+          8: ['Flight'],
+        },
+        usee: {
+          7: ['Mafia'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Subway')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        hand: ['Flight'],
+      },
+    })
+  })
+
+  test('dogma: only 6', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        yellow: {
+          cards: ['Subway', 'Canning', 'Masonry', 'Fermenting', 'Machinery'],
+          splay: 'left',
+        }
+      },
+      decks: {
+        base: {
+          8: ['Flight'],
+        },
+        usee: {
+          7: ['Mafia'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Subway')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        hand: ['Flight'],
       },
     })
   })
