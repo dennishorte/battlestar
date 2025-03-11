59e49c1cc094dafefad94c7ba4ea7c34e53e058b
dennis <dennis.horte@gmail.com>
2025-03-10 18:27:23 -0700

    Slot Machine

--- common/ultimate/res/usee/SlotMachine.js
+++ common/ultimate/res/usee/SlotMachine.js
@@ -1,4 +1,5 @@
 const CardBase = require(`../CardBase.js`)
+const { GameOverEvent } = require('../../../lib/game.js')
 
 function Card() {
   this.id = `Slot Machine`  // Card names are unique in Innovation
@@ -6,7 +7,7 @@ function Card() {
   this.color = `purple`
   this.age = 7
   this.expansion = `usee`
-  this.biscuits = `fiih` 
+  this.biscuits = `fiih`
   this.dogmaBiscuit = `i`
   this.inspire = ``
   this.echo = ``
@@ -19,33 +20,34 @@ function Card() {
     (game, player) => {
       const drawnCards = [
         game.aDrawAndReveal(player, game.getEffectAge(this, 1)),
-        game.aDrawAndReveal(player, game.getEffectAge(this, 2)), 
+        game.aDrawAndReveal(player, game.getEffectAge(this, 2)),
         game.aDrawAndReveal(player, game.getEffectAge(this, 3)),
         game.aDrawAndReveal(player, game.getEffectAge(this, 4)),
         game.aDrawAndReveal(player, game.getEffectAge(this, 5))
       ]
-      
+
       const numGreen = drawnCards.filter(card => card.color === 'green').length
-      
-      if (numGreen >= 1) {
-        game.aChooseAndSplay(player, ['green', 'purple'], 'right')
-      }
-      
-      if (numGreen >= 2) {
-        game.aScoreMany(player, drawnCards)
-      } else {
-        game.aReturnMany(player, drawnCards)
-      }
 
       if (numGreen >= 3) {
         throw new GameOverEvent({ player, reason: this.name })
       }
+
+      if (numGreen >= 1) {
+        game.aChooseAndSplay(player, ['green', 'purple'], 'right', { count: 1 })
+      }
+
+      if (numGreen >= 2) {
+        game.aScoreMany(player, drawnCards)
+      }
+      else {
+        game.aReturnMany(player, drawnCards)
+      }
     },
   ]
-  
+
   this.echoImpl = []
   this.inspireImpl = []
-  this.karmaImpl = [] 
+  this.karmaImpl = []
 }
 
 Card.prototype = Object.create(CardBase.prototype)
--- common/ultimate/res/usee/SlotMachine.test.js
+++ common/ultimate/res/usee/SlotMachine.test.js
@@ -2,11 +2,22 @@ Error.stackTraceLimit = 100
 const t = require('../../testutil.js')
 describe('Slot Machine', () => {
 
-  test('dogma', () => {
+  test('dogma: 3 greens', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
     t.setBoard(game, {
       dennis: {
         purple: ['Slot Machine'],
+      },
+      decks: {
+        base: {
+          2: ['Monotheism'],
+          3: ['Paper'],
+          4: ['Navigation'],
+          5: ['Coal'],
+        },
+        usee: {
+          1: ['Dance'],
+        },
       },
     })
 
@@ -14,10 +25,76 @@ describe('Slot Machine', () => {
     request = game.run()
     request = t.choose(game, request, 'Dogma.Slot Machine')
 
+    t.testGameOver(request, 'dennis', 'Slot Machine')
+  })
+
+  test('dogma: 2 greens', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        purple: ['Slot Machine', 'Astronomy'],
+      },
+      decks: {
+        base: {
+          2: ['Monotheism'],
+          3: ['Paper'],
+          4: ['Reformation'],
+          5: ['Coal'],
+        },
+        usee: {
+          1: ['Dance'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Slot Machine')
+    request = t.choose(game, request, 'auto')
+
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
-        purple: ['Slot Machine'],
+        purple: {
+          cards: ['Slot Machine', 'Astronomy'],
+          splay: 'right',
+        },
+        score: ['Dance', 'Monotheism', 'Paper', 'Reformation', 'Coal'],
+      },
+    })
+  })
+
+  test('dogma: 1 green', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        purple: ['Slot Machine', 'Astronomy'],
+      },
+      decks: {
+        base: {
+          2: ['Monotheism'],
+          3: ['Optics'],
+          4: ['Reformation'],
+          5: ['Coal'],
+        },
+        usee: {
+          1: ['Dance'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Slot Machine')
+    request = t.choose(game, request, 'auto')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        purple: {
+          cards: ['Slot Machine', 'Astronomy'],
+          splay: 'right',
+        },
       },
     })
   })
