76259c3bbf236e01ed43a744f0117c1d2c189178
dennis <dennis.horte@gmail.com>
2025-03-11 15:10:43 -0700

    Scouting

--- common/ultimate/res/usee/Scouting.js
+++ common/ultimate/res/usee/Scouting.js
@@ -3,7 +3,7 @@ const CardBase = require(`../CardBase.js`)
 function Card() {
   this.id = `Scouting`  // Card names are unique in Innovation
   this.name = `Scouting`
-  this.color = `blue` 
+  this.color = `blue`
   this.age = 8
   this.expansion = `usee`
   this.biscuits = `lssh`
@@ -12,22 +12,28 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `Draw and reveal two {8}. Return at least one of the drawn cards. If you return two, reveal the top card of the {10} deck. If the color of the revealed card matches the color of at least one of the returned cards, draw a {10}.`
+    `Draw and reveal two {9}. Return at least one of the drawn cards. If you return two, draw and reveal a {0}. If the color of the revealed card matches the color of at least one of the returned cards, keep it. Otherwise, put it back on top of its deck.`
   ]
 
   this.dogmaImpl = [
     (game, player) => {
-      const cardsDrawn = []
-      cardsDrawn.push(game.aDrawAndReveal(player, game.getEffectAge(this, 8)))
-      cardsDrawn.push(game.aDrawAndReveal(player, game.getEffectAge(this, 8)))
+      const cardsDrawn = [
+        game.aDrawAndReveal(player, game.getEffectAge(this, 9)),
+        game.aDrawAndReveal(player, game.getEffectAge(this, 9)),
+      ].filter(x => x !== undefined)
 
       const returned = game.aChooseAndReturn(player, cardsDrawn, { min: 1, max: 2 })
 
       if (returned.length === 2) {
-        const revealedCard = game.mReveal(game, game.aDraw(game, { age: 10 })[0])
-
-        if (returned.some(card => card.color === revealedCard.color)) {
-          game.mMoveCardTo(revealedCard, game.getZoneByPlayer(player, 'hand'))
+        const card = game.aDrawAndReveal(player, game.getEffectAge(this, 10))
+        if (cardsDrawn.find(c => c.color === card.color)) {
+          game.mLog({
+            template: '{player} keeps the card',
+            args: { player }
+          })
+        }
+        else {
+          game.mMoveCardToTop(card, game.getZoneByCardHome(card), { player })
         }
       }
     },
--- common/ultimate/res/usee/Scouting.test.js
+++ common/ultimate/res/usee/Scouting.test.js
@@ -2,22 +2,97 @@ Error.stackTraceLimit = 100
 const t = require('../../testutil.js')
 describe('Scouting', () => {
 
-  test('dogma', () => {
+  test('dogma, return one', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
     t.setBoard(game, {
       dennis: {
         blue: ['Scouting'],
+      },
+      decks: {
+        base: {
+          9: ['Computers'],
+        },
+        usee: {
+          9: ['Teleprompter'],
+        },
       },
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Scouting')
+    request = t.choose(game, request, 'Teleprompter')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
         blue: ['Scouting'],
+        hand: ['Computers'],
+      },
+    })
+  })
+
+  test('dogma, return two; no match', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        blue: ['Scouting'],
+      },
+      decks: {
+        base: {
+          9: ['Computers'],
+          10: ['Robotics'],
+        },
+        usee: {
+          9: ['Teleprompter'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Scouting')
+    request = t.choose(game, request, 'Teleprompter', 'Computers')
+    request = t.choose(game, request, 'auto')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        blue: ['Scouting'],
+      },
+    })
+
+    expect(game.getZoneByDeck('base', 10).cards()[0].name).toBe('Robotics')
+  })
+
+  test('dogma, return two; match', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        blue: ['Scouting'],
+      },
+      decks: {
+        base: {
+          9: ['Computers'],
+          10: ['Software'],
+        },
+        usee: {
+          9: ['Teleprompter'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Scouting')
+    request = t.choose(game, request, 'Teleprompter', 'Computers')
+    request = t.choose(game, request, 'auto')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        blue: ['Scouting'],
+        hand: ['Software'],
       },
     })
   })
