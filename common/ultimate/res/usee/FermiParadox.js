60ca7c8b63b51441cbd5043270da2b3a7257fff2
dennis <dennis.horte@gmail.com>
2025-03-12 16:21:42 -0700

    Fermi Paradox

--- common/ultimate/res/usee/FermiParadox.js
+++ common/ultimate/res/usee/FermiParadox.js
@@ -7,53 +7,50 @@ function Card() {
   this.color = `blue`
   this.age = 9
   this.expansion = `usee`
-  this.biscuits = `hiis` 
+  this.biscuits = `hiis`
   this.dogmaBiscuit = `i`
   this.inspire = ``
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `Reveal the top card of the {9} deck and the {10} deck. Return the top card of the {9} deck or the {10} deck.`,
+    `Reveal the top card of the {9} deck and the {0} deck. Return the top card of the {9} deck or the {0} deck.`,
     `If you have no cards on your board, you win. Otherwise, transfer all valued cards in the junk to your hand.`
   ]
 
   this.dogmaImpl = [
     (game, player) => {
-      const age9Card = game.aReveal(player, game.getZoneById('deck9').cards().slice(-1)[0])
-      const age10Card = game.aReveal(player, game.getZoneById('deck10').cards().slice(-1)[0])
+      const cards = [
+        game.getZoneByDeck('base', game.getEffectAge(this, 9)).cards()[0],
+        game.getZoneByDeck('base', game.getEffectAge(this, 10)).cards()[0],
+      ].filter(x => x)
 
-      const cardsToChoose = [age9Card, age10Card].filter(card => card !== undefined)
-      const cardToReturn = game.aChooseAndReturn(player, cardsToChoose)[0]
-      
-      game.mLog({
-        template: '{player} returned {card}',
-        args: { player, card: cardToReturn }
+      cards.forEach(card => game.mReveal(player, card))
+
+      game.aChooseAndReturn(player, cards, {
+        title: 'Choose a card to put on the bottom of its deck',
       })
     },
 
     (game, player) => {
-      const numBoardCards = game
-        .getPlayerBoard(player)
-        .flatMap(stack => stack.cards())
-        .length
+      const numBoardCards = game.getTopCards(player).length
 
       if (numBoardCards === 0) {
         throw new GameOverEvent({
-          player, 
-          reason: 'Fermi Paradox'
+          player,
+          reason: this.name,
         })
       }
       else {
         const valuedJunkCards = game
           .getZoneById('junk')
           .cards()
-          .filter(card => card.age > 0)
+          .filter(card => card.age !== undefined)
 
-        game.aTransferMany(player, valuedJunkCards, game.getZoneByPlayer(player, 'hand'))
+        game.aTransferMany(player, valuedJunkCards, game.getZoneByPlayer(player, 'hand'), { ordered: true })
       }
     }
   ]
-  
+
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
--- common/ultimate/res/usee/FermiParadox.test.js
+++ common/ultimate/res/usee/FermiParadox.test.js
@@ -8,18 +8,51 @@ describe('Fermi Paradox', () => {
       dennis: {
         blue: ['Fermi Paradox'],
       },
+      junk: ['Monument', 'Tools', 'Optics'],
+      decks: {
+        base: {
+          9: ['Specialization'],
+          10: ['Software', 'Robotics'],
+        },
+      },
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Fermi Paradox')
+    request = t.choose(game, request, 'Software')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
         blue: ['Fermi Paradox'],
+        hand: ['Tools', 'Optics'],
       },
     })
+
+    expect(game.getZoneByDeck('base', 10).cards()[0].name).toBe('Robotics')
+  })
+
+  test('dogma: you win', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee', 'arti'] })
+    t.setBoard(game, {
+      dennis: {
+        artifact: ['Fermi Paradox'],
+      },
+      decks: {
+        base: {
+          9: ['Specialization'],
+          10: ['Software', 'Robotics'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'dogma')
+    request = t.choose(game, request, 'Software')
+
+    t.testGameOver(request, 'dennis', 'Fermi Paradox')
   })
 
 })
