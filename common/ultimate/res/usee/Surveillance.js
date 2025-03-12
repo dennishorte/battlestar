a8e77e51f41643f2061356598a6a2894bfc083f8
dennis <dennis.horte@gmail.com>
2025-03-12 13:09:02 -0700

    Surveillance

--- common/ultimate/res/usee/Surveillance.js
+++ common/ultimate/res/usee/Surveillance.js
@@ -2,11 +2,11 @@ const CardBase = require(`../CardBase.js`)
 const { GameOverEvent } = require('../../../lib/game.js')
 
 function Card() {
-  this.id = `Surveillance`  
+  this.id = `Surveillance`
   this.name = `Surveillance`
   this.color = `yellow`
   this.age = 9
-  this.expansion = `usee` 
+  this.expansion = `usee`
   this.biscuits = `siih`
   this.dogmaBiscuit = `i`
   this.inspire = ``
@@ -14,25 +14,31 @@ function Card() {
   this.karma = []
   this.dogma = [
     `I demand you reveal your hand! If each color present in my hand is present in yours, and vice versa, and your hand is not empty, I win!`,
-    `Draw a {10}.`
+    `Draw a {0}.`
   ]
 
   this.dogmaImpl = [
     (game, player, { leader }) => {
       const leaderHand = game.getCardsByZone(leader, 'hand')
       const leaderColors = [...new Set(leaderHand.map(card => card.color))]
-      
+
       const playerHand = game.getCardsByZone(player, 'hand')
       const playerColors = [...new Set(playerHand.map(card => card.color))]
-      
-      game.mReveal(player, playerHand)
-      
-      if (playerHand.length > 0 &&
-          leaderColors.every(color => playerColors.includes(color)) && 
+
+      if (playerHand.length === 0) {
+        game.mLogNoEffect()
+        return
+      }
+
+      game.aRevealMany(player, playerHand, { ordered: true })
+      game.aRevealMany(player, leaderHand, { ordered: true })
+
+
+      if (leaderColors.every(color => playerColors.includes(color)) &&
           playerColors.every(color => leaderColors.includes(color))) {
         throw new GameOverEvent({
           player: leader,
-          reason: this.name 
+          reason: this.name
         })
       }
     },
--- common/ultimate/res/usee/Surveillance.test.js
+++ common/ultimate/res/usee/Surveillance.test.js
@@ -2,11 +2,16 @@ Error.stackTraceLimit = 100
 const t = require('../../testutil.js')
 describe('Surveillance', () => {
 
-  test('dogma', () => {
+  test('dogma: empty hands', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
     t.setBoard(game, {
       dennis: {
         yellow: ['Surveillance'],
+      },
+      decks: {
+        usee: {
+          10: ['Fight Club'],
+        },
       },
     })
 
@@ -18,6 +23,64 @@ describe('Surveillance', () => {
     t.testBoard(game, {
       dennis: {
         yellow: ['Surveillance'],
+        hand: ['Fight Club'],
+      },
+    })
+  })
+
+  test('dogma: colors match', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        yellow: ['Surveillance'],
+        hand: ['Tools', 'The Wheel', 'Paper'],
+      },
+      micah: {
+        hand: ['Experimentation', 'Metric System'],
+      },
+      decks: {
+        usee: {
+          10: ['Fight Club'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Surveillance')
+
+    t.testGameOver(request, 'dennis', 'Surveillance')
+  })
+
+  test('dogma: mismatch', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'usee'] })
+    t.setBoard(game, {
+      dennis: {
+        yellow: ['Surveillance'],
+        hand: ['Tools', 'The Wheel', 'Paper'],
+      },
+      micah: {
+        hand: ['Experimentation'],
+      },
+      decks: {
+        usee: {
+          10: ['Fight Club'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Dogma.Surveillance')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        yellow: ['Surveillance'],
+        hand: ['Fight Club', 'Tools', 'The Wheel', 'Paper'],
+      },
+      micah: {
+        hand: ['Experimentation'],
       },
     })
   })
