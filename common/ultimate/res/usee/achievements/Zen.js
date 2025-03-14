2fabcd3bcc9ddbb3b6b6302c8fe72a33c21377ea
dennis <dennis.horte@gmail.com>
2025-03-14 13:38:56 -0700

    Zen

--- common/ultimate/res/usee/achievements/Zen.js
+++ common/ultimate/res/usee/achievements/Zen.js
@@ -5,11 +5,23 @@ function Card() {
   this.name = 'Zen'
   this.shortName = 'zen'
   this.expansion = 'usee'
-  this.text = 'Have a topc card on your board of value 6 or higher and no top card on your board of odd value.'
+  this.text = 'Have a top card on your board of value 6 or higher and no top card on your board of odd value.'
   this.alt = 'Meteorology'
   this.isSpecialAchievement = true
   this.checkPlayerIsEligible = function(game, player, reduceCost) {
-    return false
+    const targetAge = reduceCost ? 5 : 6
+    const topCardAges = game
+      .getTopCards(player)
+      .map(card => card.getAge())
+    const topCardMaxAge = Math.max(...topCardAges)
+
+    const targetOdd = reduceCost ? 1 : 0
+    const numOdd = game
+      .getTopCards(player)
+      .filter(card => card.getAge() % 2 === 1)
+      .length
+
+    return topCardMaxAge >= targetAge && numOdd <= targetOdd
   }
 }
 
