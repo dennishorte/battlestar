12728d9c56cb837c5aad4087a543a9c554a57ae1
dennis <dennis.horte@gmail.com>
2025-03-19 09:41:09 -0700

    With Mystery Box, reveal the card before self executing it

--- common/ultimate/res/usee/MysteryBox.js
+++ common/ultimate/res/usee/MysteryBox.js
@@ -24,6 +24,7 @@ function Card() {
 
       if (achievement) {
         game.aClaimAchievement(player, achievement)
+        game.mReveal(player, achievement)
         game.aSelfExecute(player, achievement)
       }
     },
