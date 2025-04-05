3d7866e2c0c59f7b17ff1f4d9db0fd7a7c568e4f
dennis <dennis.horte@gmail.com>
2025-04-05 08:45:12 -0700

    Almanac

--- common/ultimate/res/echo/Almanac.js
+++ common/ultimate/res/echo/Almanac.js
@@ -8,10 +8,11 @@ function Card() {
   this.expansion = `echo`
   this.biscuits = `hl4&`
   this.dogmaBiscuit = `l`
-  this.echo = `Draw and foreshadow a {4}.`
+  this.echo = `Draw and foreshadow an Echoes {4}.`
   this.karma = []
   this.dogma = [
-    `You may return a card from your forecast with a bonus. If you do, draw and score a card of value one higher than that bonus.`
+    `You may return a card from your forecast with a bonus. If you do, draw and score a card of value one higher than that bonus.`,
+    `If Almanac was foreseen, foreshadow all cards in another player's forecast.`,
   ]
 
   this.dogmaImpl = [
@@ -26,10 +27,23 @@ function Card() {
         const bonuses = card.getBonuses()
         game.aDrawAndScore(player, bonuses[0] + 1)
       }
-    }
+    },
+
+    (game, player, { foreseen, self }) => {
+      if (foreseen) {
+        game.mLogWasForeseen(self)
+
+        const others = game.getPlayerAll().filter(o => o.name !== player.name)
+        const other = game.aChoosePlayer(player, others)
+        game.aForeshadowMany(player, game.getCardsByZone(other, 'forecast'))
+      }
+      else {
+        game.mLogNoEffect()
+      }
+    },
   ]
   this.echoImpl = (game, player) => {
-    game.aDrawAndForeshadow(player, game.getEffectAge(this, 4))
+    game.aDrawAndForeshadow(player, game.getEffectAge(this, 4), { exp: 'echo' })
   }
   this.karmaImpl = []
 }
--- common/ultimate/res/echo/Almanac.test.js
+++ common/ultimate/res/echo/Almanac.test.js
@@ -8,13 +8,16 @@ describe("Almanac", () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
     t.setBoard(game,  {
       dennis: {
+        yellow: ['Machinery'],
         blue: ['Almanac'],
         forecast: ['Horseshoes'],
       },
       decks: {
         base: {
-          3: ['Machinery'],
-          4: ['Gunpowder'],
+          3: ['Engineering'],
+        },
+        echo: {
+          4: ['Toilet'],
         },
       },
     })
@@ -27,9 +30,47 @@ describe("Almanac", () => {
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
+        yellow: ['Machinery'],
         blue: ['Almanac'],
-        score: ['Machinery'],
-        forecast: ['Gunpowder'],
+        score: ['Engineering'],
+        forecast: ['Toilet'],
+      },
+    })
+  })
+
+  test('dogma: was forseen', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        hand: ['Machinery'],
+        forecast: ['Horseshoes', 'Almanac'],
+      },
+      micah: {
+        forecast: ['Software'],
+      },
+      decks: {
+        base: {
+          3: ['Engineering'],
+        },
+        echo: {
+          4: ['Toilet'],
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Machinery')
+    request = t.choose(game, request, 'Almanac')
+    request = t.choose(game, request, 'Horseshoes')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        yellow: ['Machinery'],
+        blue: ['Almanac'],
+        score: ['Engineering'],
+        forecast: ['Toilet', 'Software'],
       },
     })
   })
