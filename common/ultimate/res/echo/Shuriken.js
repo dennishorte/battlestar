edb91f78c21d30516942f4a8ab5556d5b1551252
dennis <dennis.horte@gmail.com>
2025-04-05 15:56:05 -0700

    Shuriken

--- common/ultimate/res/echo/Shuriken.js
+++ common/ultimate/res/echo/Shuriken.js
@@ -11,21 +11,34 @@ function Card() {
   this.echo = ``
   this.karma = []
   this.dogma = [
-    `I demand you transfer a top non-red card with a {k} or {s} from your board to my board! If you do, draw a {4}.`,
+    `I demand you transfer two non-red top cards with {k} or {p} of different colors from your board to my board! If you do, and Shuriken was foreseen, transfer them to my achievements!`,
     `You may splay your purple cards right.`
   ]
 
   this.dogmaImpl = [
-    (game, player, { leader }) => {
+    (game, player, { leader, foreseen, self }) => {
       const choices = game
         .getTopCards(player)
         .filter(card => card.color !== 'red')
-        .filter(card => card.checkHasBiscuit('k') || card.checkHasBiscuit('s'))
-      const card = game.aChooseCard(player, choices)
-      if (card) {
-        const transferred = game.aTransfer(player, card, game.getZoneByPlayer(leader, card.color))
-        if (transferred) {
-          game.aDraw(player, { age: game.getEffectAge(this, 4) })
+        .filter(card => card.checkHasBiscuit('k') || card.checkHasBiscuit('p'))
+
+      let cards = game.aChooseCards(player, choices, { count: 2 })
+
+      if (cards.length > 0) {
+        const transferred = []
+        while (cards.length > 0) {
+          const card = game.aChooseCard(player, cards)
+          cards = cards.filter(other => other.id !== card.id)
+
+          const trans = game.aTransfer(player, card, game.getZoneByPlayer(leader, card.color))
+          if (trans) {
+            transferred.push(trans)
+          }
+        }
+
+        if (foreseen && transferred.length === 2) {
+          game.mLogWasForeseen(self)
+          game.aTransferMany(player, transferred, game.getZoneByPlayer(leader, 'achievements'), { ordered: true })
         }
       }
     },
