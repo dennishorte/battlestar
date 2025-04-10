7ee287e0f56aa516e06e0f6fe8957aab7dae8916
dennis <dennis.horte@gmail.com>
2025-04-10 09:50:33 -0700

    Add a notif_model for interacting with the notificationThrottle collection

---
+++ api/src/models/notif_models.js
@@ -0,0 +1,45 @@
+// Database and collection
+const databaseClient = require('../util/mongo.js').client
+const database = databaseClient.db('games')
+const notifCollection = database.collection('notificationThrottle')
+
+const Notif = {}
+module.exports = Notif
+
+const cutoff = 1000 * 60 * 60 // one hour
+
+function _makeKey(user, game) {
+  return {
+    userId: user._id,
+    gameId: game._id,
+  }
+}
+
+// Find all notification throttles older than the cutoff and remove them.
+Notif.clean = async function() {
+  const oneHourAgo = Date.now() - cutoff
+  await notifCollection.deleteMany({
+    createdAt: { $lt: oneHourAgo }
+  })
+}
+
+Notif.clear = async function(user, game) {
+  await notifCollection.deleteOne(_makeKey(user, game))
+}
+
+// Return true if the notification should be throttled.
+Notif.throttleOrSet = async function(user, game) {
+  const item = await notifCollection.findOne(_makeKey(user, game))
+  const throttle = Boolean(item) && (Date.now() - item.createdAt < cutoff)
+
+  if (!throttle) {
+    // Set throttle
+    await notifCollection.updateOne(
+      _makeKey(user, game),
+      { $set: { createdAt: Date.now() } },
+      { upsert: true },
+    )
+  }
+
+  return throttle
+}
