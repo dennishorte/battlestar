bea81423b7397b6b4aead0dab9ff15cde69e37c2
dennis <dennis.horte@gmail.com>
2025-05-20 09:42:17 -0700

    Show scars in ScarRound component

--- app/src/modules/games/cube_draft/components/ScarRound.vue
+++ app/src/modules/games/cube_draft/components/ScarRound.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="alert alert-warning">
     <h3>Scar Round!</h3>
+
+    <BAlert :model-value="true" v-for="scar in props.scars" :key="scar.id">
+      {{ scar.text }}
+    </BAlert>
+
   </div>
 </template>
 
 
 <script setup>
-
+const props = defineProps({
+  scars: {
+    type: Array,
+    default: () => [],
+  },
+})
 </script>
 
 
