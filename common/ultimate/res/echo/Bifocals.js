086033f563ec8d9d60add802be71ff038e4ba5d4
dennis <dennis.horte@gmail.com>
2025-08-16 09:26:20 -0700

    Bifocals

--- common/ultimate/res/echo/Bifocals.js
+++ common/ultimate/res/echo/Bifocals.js
@@ -5,26 +5,34 @@ module.exports = {
   expansion: `echo`,
   biscuits: `&hcc`,
   dogmaBiscuit: `c`,
-  echo: `Draw and foreshadow a card of any value.`,
+  echo: `Return a card from your forecast.`,
   dogma: [
-    `You may return a card from your forecast. If you do, draw and foreshadow a card of equal value to the card returned.`,
-    `You may splay your green cards right.`
+    `Draw and foreshadow a {7}. If Bifocals was foreseen, draw and foreshadow a card of value equal to the lowest available standard achievement.`,
+    `You may splay your green cards right. If you do, splay up any color of your cards.`
   ],
   dogmaImpl: [
-    (game, player) => {
-      const returned = game.actions.chooseAndReturn(player, game.cards.byPlayer(player, 'forecast'), { min: 0, max: 1 })
+    (game, player, { foreseen, self }) => {
+      game.actions.drawAndForeshadow(player, game.getEffectAge(self, 7))
 
-      if (returned && returned.length > 0) {
-        game.actions.drawAndForeshadow(player, returned[0].getAge())
+      game.log.addForeseen(foreseen, self)
+      if (foreseen) {
+        const age = game
+          .getAvailableStandardAchievements(player)
+          .map(achievement => achievement.age)
+          .sort()[0] || 0
+
+        game.actions.drawAndForeshadow(player, age)
       }
     },
 
     (game, player) => {
-      game.actions.chooseAndSplay(player, ['green'], 'right')
+      const splayed = game.actions.chooseAndSplay(player, ['green'], 'right')
+      if (splayed) {
+        game.actions.chooseAndSplay(player, game.util.colors(), 'up', { count: 1 })
+      }
     }
   ],
   echoImpl: (game, player) => {
-    const age = game.actions.chooseAge(player)
-    game.actions.drawAndForeshadow(player, age)
+    game.actions.chooseAndReturn(player, game.cards.byPlayer(player, 'forecast'))
   },
 }
--- common/ultimate/res/echo/Bifocals.test.js
+++ common/ultimate/res/echo/Bifocals.test.js
@@ -8,34 +8,81 @@ describe("Bifocals", () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
     t.setBoard(game,  {
       dennis: {
-        blue: ['Bifocals'],
+        red: ['Industrialization'],
+        blue: ['Bifocals', 'Tools'],
         green: ['Paper', 'Sailing'],
         forecast: ['Canning'],
       },
       decks: {
         base: {
           2: ['Calendar'],
-          6: ['Industrialization'],
+          7: ['Lighting'],
         },
       },
+      achievements: ['Construction', 'Machinery'],
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Bifocals')
-    request = t.choose(game, request, 2)
-    request = t.choose(game, request, 'Canning')
     request = t.choose(game, request, 'green')
+    request = t.choose(game, request, 'blue')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
-        blue: ['Bifocals'],
+        red: ['Industrialization'],
+        blue: {
+          cards: ['Bifocals', 'Tools'],
+          splay: 'up',
+        },
         green: {
           cards: ['Paper', 'Sailing'],
           splay: 'right'
         },
-        forecast: ['Calendar', 'Industrialization'],
+        forecast: ['Lighting'],
+      },
+    })
+  })
+
+  test('dogma: foreseen', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        blue: ['Tools'],
+        green: ['Paper', 'Sailing'],
+        hand: ['Industrialization'],
+        forecast: ['Canning', 'Bifocals'],
+      },
+      decks: {
+        base: {
+          2: ['Calendar'],
+          7: ['Lighting'],
+        },
+      },
+      achievements: ['Construction', 'Machinery'],
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Industrialization')
+    request = t.choose(game, request, 'Bifocals')
+    request = t.choose(game, request, 'green')
+    request = t.choose(game, request, 'blue')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Industrialization'],
+        blue: {
+          cards: ['Bifocals', 'Tools'],
+          splay: 'up',
+        },
+        green: {
+          cards: ['Paper', 'Sailing'],
+          splay: 'right'
+        },
+        forecast: ['Lighting', 'Calendar'],
       },
     })
   })
