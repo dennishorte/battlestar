5fe1ba18b0054b01cc36917733b63c85a2596b2b
dennis <dennis.horte@gmail.com>
2025-08-16 15:42:22 -0700

    Rubber

--- common/ultimate/res/echo/Rubber.js
+++ common/ultimate/res/echo/Rubber.js
@@ -5,25 +5,35 @@ module.exports = {
   expansion: `echo`,
   biscuits: `h&f7`,
   dogmaBiscuit: `f`,
-  echo: `Draw and tuck two {7}.`,
+  echo: `Draw and tuck two {8}.`,
   dogma: [
     `Score a top card from your board without a bonus.`,
-    `You may splay your red cards up.`
+    `You may splay your red cards up.`,
+    `If Rubber was foreseen, foreshadow a top card on your board.`,
   ],
   dogmaImpl: [
     (game, player) => {
       const choices = game
-        .cards.tops(player)
+        .cards
+        .tops(player)
         .filter(card => !card.checkHasBonus())
       game.actions.chooseAndScore(player, choices)
     },
 
     (game, player) => {
       game.actions.chooseAndSplay(player, ['red'], 'up')
-    }
+    },
+
+    (game, player, { foreseen, self }) => {
+      game.log.addForeseen(foreseen, self)
+      if (foreseen) {
+        const choices = game.cards.tops(player)
+        game.actions.chooseAndForeshadow(player, choices)
+      }
+    },
   ],
   echoImpl: (game, player, { self }) => {
-    game.actions.drawAndTuck(player, game.getEffectAge(self, 7))
-    game.actions.drawAndTuck(player, game.getEffectAge(self, 7))
+    game.actions.drawAndTuck(player, game.getEffectAge(self, 8))
+    game.actions.drawAndTuck(player, game.getEffectAge(self, 8))
   },
 }
--- common/ultimate/res/echo/Rubber.test.js
+++ common/ultimate/res/echo/Rubber.test.js
@@ -11,9 +11,9 @@ describe("Rubber", () => {
         red: ['Rubber'],
       },
       decks: {
-        base: {
-          7: ['Lighting', 'Combustion'],
-        }
+        echo: {
+          8: ['Tractor', 'Parachute'],
+        },
       }
     })
 
@@ -26,10 +26,45 @@ describe("Rubber", () => {
     t.testBoard(game, {
       dennis: {
         red: {
-          cards: ['Rubber', 'Combustion'],
+          cards: ['Rubber', 'Parachute'],
           splay: 'up'
         },
-        score: ['Lighting'],
+        score: ['Tractor'],
+      },
+    })
+  })
+
+  test('dogma: was foreseen', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        red: ['Metalworking'],
+        hand: ['Credit Card'],
+        forecast: ['Rubber'],
+      },
+      decks: {
+        echo: {
+          8: ['Tractor', 'Parachute'],
+        },
+      }
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Credit Card')
+    request = t.choose(game, request, 'red')
+    request = t.choose(game, request, 'Rubber')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: {
+          cards: ['Metalworking', 'Parachute'],
+          splay: 'up'
+        },
+        green: ['Credit Card'],
+        score: ['Tractor'],
+        forecast: ['Rubber'],
       },
     })
   })
