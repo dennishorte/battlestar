1c6f766d1b15472556a70b77b291bb9d4a019b23
dennis <dennis.horte@gmail.com>
2025-08-18 12:34:26 -0700

    Telegraph

--- common/ultimate/res/echo/Telegraph.test.js
+++ common/ultimate/res/echo/Telegraph.test.js
@@ -27,7 +27,7 @@ describe("Telegraph", () => {
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Telegraph')
-    request = t.choose(game, request, 'green right')
+    request = t.choose(game, request, 'micah.green right')
     request = t.choose(game, request, 'blue')
 
     t.testIsSecondPlayer(game)
@@ -36,6 +36,61 @@ describe("Telegraph", () => {
         green: {
           cards: ['Telegraph', 'Metric System'],
           splay: 'right'
+        },
+        blue: {
+          cards: ['Atomic Theory', 'Mathematics'],
+          splay: 'up'
+        },
+        yellow: ['Masonry', 'Agriculture'],
+      },
+      micah: {
+        red: {
+          cards: ['Archery', 'Construction'],
+          splay: 'left'
+        },
+        green: {
+          cards: ['Navigation', 'Sailing'],
+          splay: 'right'
+        },
+      },
+    })
+  })
+
+  test('dogma: was foreseen', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        green: ['Metric System'],
+        yellow: ['Masonry', 'Agriculture'],
+        blue: ['Atomic Theory', 'Mathematics'],
+        hand: ['Flight'],
+        forecast: ['Telegraph'],
+      },
+      micah: {
+        red: {
+          cards: ['Archery', 'Construction'],
+          splay: 'left'
+        },
+        green: {
+          cards: ['Navigation', 'Sailing'],
+          splay: 'right'
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Flight')
+    request = t.choose(game, request, 'micah.green right')
+    request = t.choose(game, request, 'blue')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Flight'],
+        green: {
+          cards: ['Telegraph', 'Metric System'],
+          splay: 'up'
         },
         blue: {
           cards: ['Atomic Theory', 'Mathematics'],
