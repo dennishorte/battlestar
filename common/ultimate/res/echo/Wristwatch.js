901152b6807ce95a39557834178730b45b7508f8
dennis <dennis.horte@gmail.com>
2025-08-26 16:21:35 -0700

    Wristwatch

--- common/ultimate/res/echo/Wristwatch.js
+++ common/ultimate/res/echo/Wristwatch.js
@@ -5,22 +5,35 @@ module.exports = {
   expansion: `echo`,
   biscuits: `hfa&`,
   dogmaBiscuit: `f`,
-  echo: `Take a non-yellow top card from your board and tuck it.`,
+  echo: `Tuck a top card from your board.`,
   dogma: [
-    `For each visible bonus on your board, draw and tuck a card of that value, in ascending order.`
+    `If Wristwatch was foreseen, return all non-bottom cards from your board.`,
+    `For each value in ascending order, if you have a bonus of that value, draw and meld a card of that value.`
   ],
   dogmaImpl: [
+    (game, player, { foreseen, self }) => {
+      game.log.addForeseen(foreseen, self)
+      if (foreseen) {
+        const toReturn = game
+          .util
+          .colors()
+          .map(color => game.cards.byPlayer(player, color))
+          .flatMap(stack => stack.slice(0, -1))
+
+        game.actions.returnMany(player, toReturn)
+      }
+    },
+
     (game, player) => {
-      const bonuses = game.getBonuses(player).sort((l, r) => l - r)
-      for (const bonus of bonuses) {
-        game.actions.drawAndTuck(player, bonus)
+      const bonuses = game.getBonuses(player)
+      const ages = game.util.ages().filter(age => bonuses.includes(age))
+      for (const age of ages) {
+        game.actions.drawAndMeld(player, age)
       }
     }
   ],
   echoImpl: (game, player) => {
-    const choices = game
-      .cards.tops(player)
-      .filter(card => card.color !== 'yellow')
+    const choices = game.cards.tops(player)
     game.actions.chooseAndTuck(player, choices)
   },
 }
--- common/ultimate/res/echo/Wristwatch.test.js
+++ common/ultimate/res/echo/Wristwatch.test.js
@@ -8,26 +8,65 @@ describe("Wristwatch", () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
     t.setBoard(game,  {
       dennis: {
-        yellow: ['Wristwatch'],
         red: ['Coal', 'Plumbing'],
+        yellow: ['Wristwatch'],
+        green: ['Credit Card'],
       },
       decks: {
         base: {
           2: ['Fermenting'],
           10: ['Software'],
-        }
+        },
+        echo: {
+          9: ['Rock'],
+        },
       }
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Wristwatch')
+    request = t.choose(game, request, 'Coal')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
-        yellow: ['Wristwatch', 'Fermenting'],
         red: ['Plumbing', 'Coal'],
+        yellow: ['Fermenting', 'Wristwatch'],
+        green: ['Credit Card'],
+        blue: ['Software'],
+        purple: ['Rock'],
+      },
+    })
+  })
+
+  test('dogma: was foreseen', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        red: ['Coal', 'Plumbing'],
+        hand: ['Credit Card'],
+        forecast: ['Wristwatch'],
+      },
+      decks: {
+        base: {
+          9: ['Fission'],
+          10: ['Software'],
+        },
+      }
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Credit Card')
+    request = t.choose(game, request, 'Coal')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Fission', 'Coal'],
+        yellow: ['Wristwatch'],
+        green: ['Credit Card'],
         blue: ['Software'],
       },
     })
