03cffbc8cd0a3e0eb671e3fb0d54e6c157050c18
dennis <dennis.horte@gmail.com>
2025-09-04 14:21:01 -0700

    Exoskeleton

--- common/ultimate/res/echo/Exoskeleton.js
+++ common/ultimate/res/echo/Exoskeleton.js
@@ -10,8 +10,32 @@ module.exports = {
     `You may achieve a card from any player's hand, if eligible. If you do, and Exoskeleton was foreseen, repeat this effect.`,
   ],
   dogmaImpl: [
-    (game, player) => {
-      // Implementation would go here
-    }
+    (game, player, { leader }) => {
+      const cardsInHand = game.cards.byPlayer(player, 'hand')
+      const lowestCards = game.util.lowestCards(cardsInHand)
+      const toTransfer = cardsInHand.filter(c => !lowestCards.includes(c))
+
+      game.actions.transferMany(player, toTransfer, game.zones.byPlayer(leader, 'score'))
+    },
+
+    (game, player, { foreseen, self }) => {
+      game.log.addForeseen(foreseen, self)
+      while (true) {
+        const mayAchieve = game
+          .players
+          .all()
+          .flatMap(p => game.cards.byPlayer(p, 'hand'))
+          .filter(card => game.checkAchievementEligibility(player, card))
+
+        const achieved = game.actions.chooseAndAchieve(player, mayAchieve)
+
+        if (foreseen && achieved.length > 0) {
+          continue
+        }
+        else {
+          break
+        }
+      }
+    },
   ],
 }
--- common/ultimate/res/echo/Exoskeleton.test.js
+++ common/ultimate/res/echo/Exoskeleton.test.js
@@ -8,28 +8,57 @@ describe("Exoskeleton", () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
     t.setBoard(game,  {
       dennis: {
-        green: ['Barcode'],
         red: ['Exoskeleton'],
       },
-      decks: {
-        base: {
-          3: ['Engineering'],
-          4: ['Colonialism'],
-        },
+      micah: {
+        hand: ['Agriculture', 'Bangle', 'Construction', 'Canning'],
       },
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Dogma.Exoskeleton')
-    request = t.choose(game, request, 4)
+    request = t.choose(game, request, 'auto')
+    request = t.choose(game, request, '**base-1* (micah)')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
-        red: ['Colonialism', 'Engineering'],
-        green: ['Barcode'],
         red: ['Exoskeleton'],
+        score: ['Construction', 'Canning'],
+        achievements: ['Agriculture'],
+      },
+      micah: {
+        hand: ['Bangle'],
+      },
+    })
+  })
+
+  test('dogma: foreseen', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    t.setBoard(game,  {
+      dennis: {
+        hand: ['Robocar'],
+        forecast: ['Exoskeleton'],
+      },
+      micah: {
+        hand: ['Agriculture', 'Bangle', 'Construction', 'Canning', 'Software'],
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Robocar')
+    request = t.choose(game, request, 'auto')
+    request = t.choose(game, request, '**base-1* (micah)')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Exoskeleton'],
+        green: ['Robocar'],
+        score: ['Construction', 'Canning', 'Software'],
+        achievements: ['Agriculture', 'Bangle'],
       },
     })
   })
