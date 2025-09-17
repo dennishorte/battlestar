a58934fb2926c3b3e352c7358b05d0dc18cab528
dennis <dennis.horte@gmail.com>
2025-09-16 22:56:15 -0700

    Treaty of Kadesh

--- common/ultimate/res/arti/TreatyofKadesh.js
+++ common/ultimate/res/arti/TreatyofKadesh.js
@@ -6,20 +6,22 @@ module.exports = {
   biscuits: `ckhk`,
   dogmaBiscuit: `k`,
   dogma: [
-    `I compel you to return all top cards from your board with a demand effect!`,
-    `Score a top, non-blue card from your board with a demand effect.`
+    `I compel you to return a top card with a demand effect of each color on your board!`,
+    `Score a top, non-blue card with a demand effect from your board.`
   ],
   dogmaImpl: [
     (game, player) => {
       const toReturn = game
-        .cards.tops(player)
+        .cards
+        .tops(player)
         .filter(card => card.checkHasDemand())
       game.actions.returnMany(player, toReturn)
     },
 
     (game, player) => {
       const choices = game
-        .cards.tops(player)
+        .cards
+        .tops(player)
         .filter(card => card.checkHasDemand())
         .filter(card => card.color !== 'blue')
       game.actions.chooseAndScore(player, choices)
