c6a1e8b3cf5de79b56ac3ecbcef4941b45bbccf6
dennis <dennis.horte@gmail.com>
2025-10-18 14:03:38 -0700

    Colt Paterson Revolver

--- common/ultimate/res/arti/ColtPatersonRevolver.js
+++ common/ultimate/res/arti/ColtPatersonRevolver.js
@@ -6,12 +6,13 @@ module.exports = {
   biscuits: `fhfc`,
   dogmaBiscuit: `f`,
   dogma: [
-    // `I compel you to reveal your hand! Draw a {7}! If the color of the drawn card matches the color of any other card in your hand, return all cards in your hand and all cards in your score pile!`
-    `I compel you to draw and reveal a {7}! If the color of the drawn card matches the color of any other card in your hand, return all cards in your hand and all cards in your score pile!`
+    `I compel you to reveal your hand! Draw and reveal a {7}! If the color of the drawn card matches the color of any other card in your hand, return all cards in your hand and all cards in your score pile!`
   ],
   dogmaImpl: [
     (game, player, { self }) => {
       const preDrawHand = game.cards.byPlayer(player, 'hand')
+      game.actions.revealMany(player, preDrawHand, { ordered: true })
+
       const card = game.actions.drawAndReveal(player, game.getEffectAge(self, 7))
 
       if (preDrawHand.some(other => other.color === card.color)) {
