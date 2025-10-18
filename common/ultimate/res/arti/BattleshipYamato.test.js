b2653d96b25c1f9316b8d8abc64a51899323e0fc
dennis <dennis.horte@gmail.com>
2025-10-18 14:57:50 -0700

    Battleship Yamato

--- common/ultimate/res/arti/BattleshipYamato.test.js
+++ common/ultimate/res/arti/BattleshipYamato.test.js
@@ -11,35 +11,22 @@ describe('Battleship Yamato', () => {
         red: ['Battleship Yamato'],
         score: ['Sailing'],
       },
+      decks: {
+        base: {
+          11: ['Astrogeology'],
+        }
+      }
     })
 
     let request
     request = game.run()
     request = t.choose(game, request, 'Draw.draw a card')
 
-    t.testGameOver(request, 'dennis', 'high draw')
-  })
-
-  test('drawing an artifact off Battleship Yamato', () => {
-    // Hint: you can't do it.
-    // The rules state that if there is no artifact of the appropriate age, you can't draw one.
-
-    const game = t.fixtureFirstPlayer({ expansions: ['base', 'arti'] })
-    t.setBoard(game,  {
-      dennis: {
-        red: ['Battleship Yamato'],
-        hand: ['Optics'],
-      },
-    })
-
-    let request
-    request = game.run()
-    request = t.choose(game, request, 'Meld.Optics')
-
     t.testBoard(game,  {
       dennis: {
-        red: ['Optics', 'Battleship Yamato'],
-        artifact: [],
+        red: ['Battleship Yamato'],
+        hand: ['Astrogeology'],
+        score: ['Sailing'],
       },
     })
   })
