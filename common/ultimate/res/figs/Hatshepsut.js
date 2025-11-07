7c2ccdd146e5227fa43521cac94eff1c59311873
dennis <dennis.horte@gmail.com>
2025-11-06 17:21:52 -0800

    Hatshepsut

--- common/ultimate/res/figs/Hatshepsut.js
+++ common/ultimate/res/figs/Hatshepsut.js
@@ -4,28 +4,30 @@ module.exports = {
   color: `green`,
   age: 1,
   expansion: `figs`,
-  biscuits: `1c*h`,
+  biscuits: `1cph`,
   dogmaBiscuit: `c`,
   karma: [
-    `If you would draw a card of value higher than 1 and you have a {1} in your hand, first return all cards from your hand and draw two cards of that value.`
+    `If you would draw a card of value higher than 1 and you have a {1} in your hand, first return all cards from your hand and draw a card of that value for each card you return.`
   ],
   karmaImpl: [
     {
       trigger: 'draw',
       kind: 'would-first',
-      matches: (game, player, { age }) => {
+      matches: (game, player, { age, self }) => {
         const ageCondition = age > 1
         const handCondition = game
-          .cards.byPlayer(player, 'hand')
-          .filter(c => c.getAge() === game.getEffectAge(this, 1))
+          .cards
+          .byPlayer(player, 'hand')
+          .filter(c => c.getAge() === game.getEffectAge(self, 1))
           .length > 0
 
         return ageCondition && handCondition
       },
       func: (game, player, { age }) => {
-        game.actions.returnMany(player, game.cards.byPlayer(player, 'hand'))
-        game.actions.draw(player, { age })
-        game.actions.draw(player, { age })
+        const returned = game.actions.returnMany(player, game.cards.byPlayer(player, 'hand'))
+        for (let i = 0; i < returned.length; i++) {
+          game.actions.draw(player, { age })
+        }
       }
     }
   ]
