55dad5842985c45d4b09de7b8eecc9b70e8f04e9
dennis <dennis.horte@gmail.com>
2025-12-07 13:24:24 -0800

    Christopher Polhem

--- common/ultimate/res/figs/ChristopherPolhem.js
+++ common/ultimate/res/figs/ChristopherPolhem.js
@@ -4,11 +4,11 @@ module.exports = {
   color: `yellow`,
   age: 5,
   expansion: `figs`,
-  biscuits: `hff*`,
+  biscuits: `hffp`,
   dogmaBiscuit: `f`,
   karma: [
     `You may issue an Expansion Decree with any two figures.`,
-    `Each {f} on your board provides two additional points towards your score.`
+    `Each {f} on your board provides one additional points towards your score for each achievement you have.`
   ],
   karmaImpl: [
     {
@@ -18,7 +18,9 @@ module.exports = {
     {
       trigger: 'calculate-score',
       func: (game, player) => {
-        return player.biscuits().f * 2
+        const factoryCount = player.biscuits().f
+        const achievementCount = game.cards.byPlayer(player, 'achievements').length
+        return factoryCount * achievementCount
       }
     },
   ]
