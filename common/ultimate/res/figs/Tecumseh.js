102194cce936a555acb13392978563e8b2ed201e
dennis <dennis.horte@gmail.com>
2025-12-10 13:43:42 -0800

    Tecumseh

--- common/ultimate/res/figs/Tecumseh.js
+++ common/ultimate/res/figs/Tecumseh.js
@@ -4,23 +4,45 @@ module.exports = {
   color: `red`,
   age: 6,
   expansion: `figs`,
-  biscuits: `fh&f`,
+  biscuits: `fhpf`,
   dogmaBiscuit: `f`,
   karma: [
-    `If you would tuck a card with a {f}, first return a top card with a {f} from another player's board.`
+    `If you would meld a card, first choose a value. Return a top card with {f} of the chosen value from each player's board. Junk an available achievement of the chosen value.`,
   ],
   karmaImpl: [
     {
-      trigger: 'tuck',
+      trigger: 'meld',
       kind: 'would-first',
-      matches: (game, player, { card }) => card.checkHasBiscuit('f'),
+      matches: () => true,
       func: (game, player) => {
+        const value = game.actions.chooseAge(player)
         const choices = game
-          .players.all()
-          .filter(other => other !== player)
-          .flatMap(player => game.cards.tops(player))
-          .filter(card => card.checkHasBiscuit('f'))
-        game.actions.chooseAndReturn(player, choices)
+          .players
+          .all()
+          .map(player => {
+            const cards = game
+              .cards
+              .tops(player)
+              .filter(card => card.getAge() === value)
+              .filter(card => card.checkHasBiscuit('f'))
+
+            return {
+              title: player.name,
+              choices: cards.map(card => card.id),
+              count: 1,
+            }
+          })
+          .filter(({ choices }) => choices.length > 0)
+
+        const chosen = game.actions.choose(player, choices, {
+          title: 'Choose a card to return for each player',
+          count: choices.length
+        })
+
+        const toReturn = chosen.map(selection => game.cards.byId(selection.selection[0]))
+        game.actions.returnMany(player, toReturn)
+
+        game.actions.junkAvailableAchievement(player, value)
       }
     }
   ]
--- common/ultimate/res/figs/Tecumseh.test.js
+++ common/ultimate/res/figs/Tecumseh.test.js
@@ -3,76 +3,183 @@ Error.stackTraceLimit = 100
 const t = require('../../testutil.js')
 
 describe('Tecumseh', () => {
-
-  test('echo', () => {
+  test('karma: meld card, return top cards with {f} from each player, junk achievement', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'] })
     t.setBoard(game, {
       dennis: {
         red: ['Tecumseh'],
-        green: ['Navigation'],
+        green: ['Banking'], // Age 5, has {f}
+        hand: ['Tools'], // Card to meld
       },
-      decks: {
-        base: {
-          6: ['Classification'],
-        }
-      }
+      micah: {
+        red: ['Coal'], // Age 5, has {f}
+      },
+      achievements: ['Statistics'], // Age 5 achievement
     })
 
     let request
     request = game.run()
-    request = t.choose(game, request, 'Dogma.Tecumseh')
+    request = t.choose(game, request, 'Meld.Tools')
+    request = t.choose(game, request, 5) // Choose age 5
+    request = t.choose(game, request, 'dennis.Banking', 'micah.Coal')
+    request = t.choose(game, request, 'auto')
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
         red: ['Tecumseh'],
-        green: ['Navigation', 'Classification'],
+        blue: ['Tools'], // Tools melded
+        green: [], // Banking returned
       },
+      micah: {
+        red: [], // Coal returned
+      },
+      junk: ['Statistics'], // Achievement junked
     })
   })
 
-  test('karma: tuck', () => {
+  test('karma: no cards with {f} of chosen age, still junk achievement', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'] })
     t.setBoard(game, {
       dennis: {
         red: ['Tecumseh'],
-        green: {
-          cards: ['Corporations', 'Electricity'],
-          splay: 'up',
-        }
+        green: ['Banking'], // Age 5, has {f}, but we'll choose age 1
+        hand: ['Tools'], // Card to meld
       },
       micah: {
-        red: ['Coal'],
-        green: ['Banking'],
+        red: ['Coal'], // Age 5, has {f}, but we'll choose age 1
       },
-      decks: {
-        base: {
-          6: ['Canning'],
-        }
-      }
+      achievements: ['The Wheel'], // Age 1 achievement
     })
 
     let request
     request = game.run()
-    request = t.choose(game, request, 'Dogma.Tecumseh')
-
-    t.testChoices(request, ['Coal', 'Banking'])
-
-    request = t.choose(game, request, 'Coal')
+    request = t.choose(game, request, 'Meld.Tools')
+    request = t.choose(game, request, 1) // Choose age 1 - no cards with {f} at age 1
 
     t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
         red: ['Tecumseh'],
-        yellow: ['Canning'],
-        green: {
-          cards: ['Corporations', 'Electricity'],
-          splay: 'up',
-        }
+        blue: ['Tools'], // Tools melded
+        green: ['Banking'], // Banking not returned (age 5, not age 1)
       },
       micah: {
-        green: ['Banking'],
+        red: ['Coal'], // Coal not returned (age 5, not age 1)
       },
+      junk: ['The Wheel'], // Achievement junked
+    })
+  })
+
+  test('karma: no achievement of chosen age available, still return cards', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'] })
+    t.setBoard(game, {
+      dennis: {
+        red: ['Tecumseh'],
+        green: ['Banking'], // Age 5, has {f}
+        hand: ['Tools'], // Card to meld
+      },
+      micah: {
+        red: ['Coal'], // Age 5, has {f}
+      },
+      achievements: ['The Wheel'], // Age 1 achievement, not age 5
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Tools')
+    request = t.choose(game, request, 5) // Choose age 5
+    request = t.choose(game, request, 'dennis.Banking', 'micah.Coal')
+    request = t.choose(game, request, 'auto')
+    // No achievement to junk, so no additional request
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Tecumseh'],
+        blue: ['Tools'], // Tools melded
+        green: [], // Banking returned
+      },
+      micah: {
+        red: [], // Coal returned
+      },
+      standardAchievements: ['The Wheel'], // Achievement not junked (wrong age)
+    })
+  })
+
+  test('karma: only some players have cards with {f} of chosen age', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'] })
+    t.setBoard(game, {
+      dennis: {
+        red: ['Tecumseh'],
+        green: ['Banking'], // Age 5, has {f}
+        hand: ['Tools'], // Card to meld
+      },
+      micah: {
+        red: ['Archery'], // Age 1, no {f}, and wrong age
+      },
+      achievements: ['Statistics'], // Age 5 achievement
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Tools')
+    request = t.choose(game, request, 5) // Choose age 5
+    request = t.choose(game, request, 'dennis.Banking') // Only dennis has a card with {f} at age 5
+    // Achievement selection happens automatically when only one option
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Tecumseh'],
+        blue: ['Tools'], // Tools melded
+        green: [], // Banking returned
+      },
+      micah: {
+        red: ['Archery'], // Archery not returned (no {f} and wrong age)
+      },
+      junk: ['Statistics'], // Achievement junked
+    })
+  })
+
+  test('karma: multiple players with cards to return', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'], numPlayers: 3 })
+    t.setBoard(game, {
+      dennis: {
+        red: ['Tecumseh'],
+        green: ['Banking'], // Age 5, has {f}
+        hand: ['Tools'], // Card to meld
+      },
+      micah: {
+        red: ['Coal'], // Age 5, has {f}
+      },
+      scott: {
+        blue: ['Physics'], // Age 5, has {f}
+      },
+      achievements: ['Statistics'], // Age 5 achievement
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Meld.Tools')
+    request = t.choose(game, request, 5) // Choose age 5
+    request = t.choose(game, request, 'dennis.Banking', 'micah.Coal', 'scott.Physics')
+    request = t.choose(game, request, 'auto')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Tecumseh'],
+        blue: ['Tools'], // Tools melded
+        green: [], // Banking returned
+      },
+      micah: {
+        red: [], // Coal returned
+      },
+      scott: {
+        blue: [], // Physics returned
+      },
+      junk: ['Statistics'], // Achievement junked
     })
   })
 })
