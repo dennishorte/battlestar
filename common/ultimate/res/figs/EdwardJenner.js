e5f85754d67a74a815a3520bb02b7cc9c5d3dabd
dennis <dennis.horte@gmail.com>
2025-12-10 16:37:01 -0800

    Edward Jenner

--- common/ultimate/res/figs/EdwardJenner.js
+++ common/ultimate/res/figs/EdwardJenner.js
@@ -4,22 +4,32 @@ module.exports = {
   color: `yellow`,
   age: 6,
   expansion: `figs`,
-  biscuits: `*llh`,
+  biscuits: `pllh`,
   dogmaBiscuit: `l`,
   karma: [
-    `If a player would successfully demand something of you, instead return a card from your hand.`
+    `If a player would execute a demand effect against you, instead return a card from your hand.`,
+    `If a player would score a card, first junk an available achievement of the same value.`
   ],
   karmaImpl: [
     {
       trigger: 'demand-success',
       triggerAll: true,
       kind: 'would-instead',
-      matches: (game, player) => {
-        return player === game.getPlayerByCard(this)
+      matches: (game, player, { owner }) => {
+        return player.id === owner.id
       },
       func: (game, player) => {
         game.actions.chooseAndReturn(player, game.cards.byPlayer(player, 'hand'))
       }
-    }
+    },
+    {
+      trigger: 'score',
+      triggerAll: true,
+      kind: 'would-first',
+      matches: () => true,
+      func: (game, player, { card }) => {
+        game.actions.junkAvailableAchievement(player, card.getAge())
+      },
+    },
   ]
 }
