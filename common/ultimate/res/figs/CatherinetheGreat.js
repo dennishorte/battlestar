c26f4db20e1f794165f7f8080b6bb2094d2af440
dennis <dennis.horte@gmail.com>
2025-12-12 09:04:08 -0800

    Catherine the Great

--- common/ultimate/res/figs/CatherinetheGreat.js
+++ common/ultimate/res/figs/CatherinetheGreat.js
@@ -4,30 +4,33 @@ module.exports = {
   color: `purple`,
   age: 6,
   expansion: `figs`,
-  biscuits: `*ssh`,
+  biscuits: `pssh`,
   dogmaBiscuit: `s`,
   karma: [
-    `Each {s} on your board provides two additional {s}.`,
-    `If you would meld a purple card, first transfer your top purple card into your hand.`
+    `Each {s} on your board provides one additional {s} and one additional {i}`,
+    `If a player would meld a card, first that player transfers their top card of the same color to their hand.`
   ],
   karmaImpl: [
     {
       trigger: 'calculate-biscuits',
       func(game, player, { biscuits }) {
-        const extra = game.utilEmptyBiscuits()
-        extra.s = biscuits.s * 2
+        const extra = game.util.emptyBiscuits()
+        extra.s = biscuits.s
+        extra.i = biscuits.s  // Each {s} provides one additional {i}
         return extra
       }
     },
 
     {
       trigger: 'meld',
-      matches(game, player, { card }) {
-        return card.color === 'purple'
-      },
-      func(game, player) {
-        const card = game.cards.top(player, 'purple')
-        game.actions.transfer(player, card, game.zones.byPlayer(player, 'hand'))
+      triggerAll: true,
+      kind: 'would-first',
+      matches: () => true,
+      func: (game, player, { card }) => {
+        const top = game.cards.top(player, card.color)
+        if (top && top !== card) {
+          game.actions.transfer(player, top, game.zones.byPlayer(player, 'hand'))
+        }
       },
     }
   ]
