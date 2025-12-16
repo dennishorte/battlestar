ca8a7e9f6da90b70c8f8d1da6c98de6c41c4eadd
dennis <dennis.horte@gmail.com>
2025-12-16 12:58:48 -0800

    Charles Darwin

--- common/ultimate/res/figs/CharlesDarwin.js
+++ common/ultimate/res/figs/CharlesDarwin.js
@@ -1,15 +1,14 @@
-
 module.exports = {
   id: `Charles Darwin`,  // Card names are unique in Innovation
   name: `Charles Darwin`,
   color: `blue`,
   age: 7,
   expansion: `figs`,
-  biscuits: `&8hs`,
+  biscuits: `p8hs`,
   dogmaBiscuit: `s`,
   karma: [
     `You may issue an Advancement Decree with any two figures.`,
-    `If you would claim an achievement, first if no other player has as many or more achievements as you, instead you win.`
+    `If you would claim an achievement, first if no other player has more achievements than you, you win. Otherwise, draw an {8}.`
   ],
   karmaImpl: [
     {
@@ -20,17 +19,21 @@ module.exports = {
       trigger: 'achieve',
       kind: 'would-first',
       matches: () => true,
-      func: (game, player) => {
+      func: (game, player, { self }) => {
         const sorted = game
-          .players.all()
+          .players
+          .all()
           .map(player => ({ player, count: game.getAchievementsByPlayer(player).total }))
           .sort((l, r) => r.count - l.count)
 
-        const mostCondition = sorted[0].player === player
-        const exclusiveCondition = sorted[0].count > sorted[1].count
+        const playerCount = game.getAchievementsByPlayer(player).total
+        const nobodyHasMoreCondition = sorted[0].count <= playerCount
 
-        if (mostCondition && exclusiveCondition) {
-          game.youWin(player, this.name)
+        if (nobodyHasMoreCondition) {
+          game.youWin(player, self.name)
+        }
+        else {
+          game.actions.draw(player, { age: game.getEffectAge(self, 8) })
         }
       }
     }
--- common/ultimate/res/figs/CharlesDarwin.test.js
+++ common/ultimate/res/figs/CharlesDarwin.test.js
@@ -4,31 +4,6 @@ const t = require('../../testutil.js')
 
 describe('Charles Darwin', () => {
 
-  test('echo', () => {
-    const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'] })
-    t.setBoard(game, {
-      dennis: {
-        blue: ['Charles Darwin'],
-      },
-      decks: {
-        base: {
-          8: ['Quantum Theory']
-        }
-      }
-    })
-
-    let request
-    request = game.run()
-    request = t.choose(game, request, 'Dogma.Charles Darwin')
-
-    t.testBoard(game, {
-      dennis: {
-        blue: ['Charles Darwin'],
-        hand: ['Quantum Theory'],
-      },
-    })
-  })
-
   test('karma: decree', () => {
     t.testDecreeForTwo('Charles Darwin', 'Advancement')
   })
@@ -46,12 +21,12 @@ describe('Charles Darwin', () => {
 
     let request
     request = game.run()
-    request = t.choose(game, request, 'Achieve.age 1')
+    request = t.choose(game, request, 'Achieve.*base-1*')
 
     t.testGameOver(request, 'dennis', 'Charles Darwin')
   })
 
-  test('karma: do not win (tied)', () => {
+  test('karma: win ties also', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'] })
     t.setBoard(game, {
       dennis: {
@@ -68,17 +43,48 @@ describe('Charles Darwin', () => {
 
     let request
     request = game.run()
-    request = t.choose(game, request, 'Achieve.age 1')
+    request = t.choose(game, request, 'Achieve.*base-1*')
 
+    t.testGameOver(request, 'dennis', 'Charles Darwin')
+  })
+
+  test('karma: otherwise, draw an 8', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'] })
+    t.setBoard(game, {
+      dennis: {
+        blue: ['Charles Darwin'],
+        score: ['Machine Tools'], // Enough score to claim age 1 achievement
+      },
+      micah: {
+        achievements: ['The Wheel'], // micah has 1 achievement, dennis has 0
+      },
+      achievements: ['Sailing'], // Age 1 achievement for dennis to claim
+      decks: {
+        base: {
+          8: ['Skyscrapers'], // Age 8 card to be drawn
+        },
+      },
+    })
+
+    let request
+    request = game.run()
+    request = t.choose(game, request, 'Achieve.*base-1*') // dennis tries to claim Sailing
+    // Karma triggers: micah has 1 achievement, dennis has 0
+    // Since micah has more, dennis draws an age 8 card instead of winning
+    // Then the achievement claim proceeds normally
+
+    t.testIsSecondPlayer(game)
     t.testBoard(game, {
       dennis: {
         blue: ['Charles Darwin'],
         score: ['Machine Tools'],
-        achievements: ['The Wheel'],
+        hand: ['Skyscrapers'], // Drew age 8 card from karma
+        achievements: ['Sailing'], // Achievement was still claimed
       },
       micah: {
-        hand: ['Fu Xi']
-      }
+        achievements: ['The Wheel'],
+        hand: ['Sargon of Akkad'], // Drew figure when dennis claimed standard achievement
+      },
     })
   })
 })
