a5631268710f420a775b21fbd1763e467af251ff
dennis <dennis.horte@gmail.com>
2025-12-20 12:00:51 -0800

    Emmy Noether

--- common/ultimate/res/figs/EmmyNoether.js
+++ common/ultimate/res/figs/EmmyNoether.js
@@ -4,10 +4,11 @@ module.exports = {
   color: `green`,
   age: 8,
   expansion: `figs`,
-  biscuits: `*iih`,
+  biscuits: `piih`,
   dogmaBiscuit: `i`,
   karma: [
-    `Each {i} on your board provides an additional number of points equal to the number of {i} on your board.`
+    `Each {i} on your board provides an additional number of points equal to the number of {i} on your board.`,
+    `If a player would score a card, first junk a card from that player's score pile.`
   ],
   karmaImpl: [
     {
@@ -16,6 +17,15 @@ module.exports = {
         const biscuits = player.biscuits()
         return biscuits.i * biscuits.i
       }
-    }
+    },
+    {
+      trigger: 'score',
+      triggerAll: true,
+      kind: 'would-first',
+      matches: () => true,
+      func: (game, player, { owner }) => {
+        game.actions.chooseAndJunk(owner, game.cards.byPlayer(player, 'score'))
+      }
+    },
   ]
 }
