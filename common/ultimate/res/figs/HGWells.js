a49f27b86025db7d39de0f7e91b2ed6aa2040d37
dennis <dennis.horte@gmail.com>
2025-12-21 13:38:12 -0800

    H. G. Wells

--- common/ultimate/res/figs/HGWells.js
+++ common/ultimate/res/figs/HGWells.js
@@ -4,29 +4,19 @@ module.exports = {
   color: `purple`,
   age: 8,
   expansion: `figs`,
-  biscuits: `l*hl`,
+  biscuits: `lphl`,
   dogmaBiscuit: `l`,
   karma: [
-    `If you would foreshadow a card, instead meld it, execute its non-demand Dogma effects for yourself only, and remove it from the game if it is still a top card on your board.`
+    `If you would dogma a card using {s} as a featured icon, instead draw and junk a {0}, then super-execute the junked card.`
   ],
   karmaImpl: [
     {
-      trigger: 'foreshadow',
+      trigger: 'dogma',
       kind: 'would-instead',
-      matches: () => true,
-      func: (game, player, { card }) => {
-        game.actions.meld(player, card)
-        game.aCardEffects(player, card, 'dogma')
-        const topCard = game.cards.top(player, card.color)
-        if (topCard === card) {
-          game.aRemove(player, card)
-        }
-        else {
-          game.log.add({
-            template: "{card} is no longer a top card on {player}'s board",
-            args: { card, player }
-          })
-        }
+      matches: (game, player, { featuredBiscuit }) => featuredBiscuit === 's',
+      func: (game, player, { self }) => {
+        const junkedCard = game.actions.drawAndJunk(player, game.getEffectAge(self, 10))
+        game.aSuperExecute(self, player, junkedCard)
       }
     }
   ]
