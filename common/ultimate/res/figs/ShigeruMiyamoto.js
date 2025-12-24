02ecaba428bcea7a4d88107a1de3aebb4441fdf3
dennis <dennis.horte@gmail.com>
2025-12-24 08:37:02 -0800

    Shigeru Miyamoto

--- common/ultimate/res/figs/ShigeruMiyamoto.js
+++ common/ultimate/res/figs/ShigeruMiyamoto.js
@@ -1,24 +1,23 @@
-
 module.exports = {
   id: `Shigeru Miyamoto`,  // Card names are unique in Innovation
   name: `Shigeru Miyamoto`,
   color: `yellow`,
   age: 10,
   expansion: `figs`,
-  biscuits: `hai&`,
+  biscuits: `haip`,
   dogmaBiscuit: `i`,
   karma: [
-    `If you would take a Dogma action and activate a card whose featured biscuit is {i}, first if you have exactly one, three, or six {i} on your board, you win.`
+    `If you would dogma a card using {i} as the featured biscuit, first if you have exactly one, three, or six {i} on your board, you win.`
   ],
   karmaImpl: [
     {
       trigger: 'dogma',
       kind: 'would-first',
       matches: (game, player, { card }) => card.checkHasBiscuit('i'),
-      func: (game, player) => {
+      func: (game, player, { self }) => {
         const clocks = player.biscuits().i
         if (clocks === 1 || clocks === 3 || clocks === 6) {
-          game.youWin(player, this.name)
+          game.youWin(player, self.name)
         }
         else {
           game.log.addNoEffect()
