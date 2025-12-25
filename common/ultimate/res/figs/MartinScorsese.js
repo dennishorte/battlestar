5acbdaf84f28ae89e402569b73ab554bf72a9eee
dennis <dennis.horte@gmail.com>
2025-12-25 11:25:06 -0800

    Martin Scorsese

--- common/ultimate/res/figs/MartinScorsese.js
+++ common/ultimate/res/figs/MartinScorsese.js
@@ -4,10 +4,10 @@ module.exports = {
   color: `purple`,
   age: 10,
   expansion: `figs`,
-  biscuits: `fha&`,
+  biscuits: `fhap`,
   dogmaBiscuit: `f`,
   karma: [
-    `If you would meld a figure, instead tuck the figure and claim a standard achievement, regardless of eligibility.`
+    `If you would meld a figure, instead tuck the figure and claim an achievement, regardless of eligibility.`
   ],
   karmaImpl: [
     {
@@ -16,18 +16,8 @@ module.exports = {
       matches: (game, player, { card }) => card.checkIsFigure(),
       func: (game, player, { card }) => {
         game.actions.tuck(player, card)
-        const choices = game
-          .getZoneById('achievements')
-          .cards()
-          .filter(card => !card.isSpecialAchievement)
-        const formatted = game.formatAchievements(choices)
-        const selected = game.actions.choose(player, formatted)[0]
-        if (selected) {
-          game.aAchieveAction(player, selected, { nonAction: true })
-        }
-        else {
-          game.log.add({ template: 'There are no available standard achievements', })
-        }
+        const choices = game.getAvailableStandardAchievements(player)
+        game.actions.chooseAndAchieve(player, choices)
       }
     }
   ]
