f8c4af0a7d72f8035eaff73575d9580404a568c5
dennis <dennis.horte@gmail.com>
2025-12-27 08:53:18 -0800

    Boyan Slat

--- common/ultimate/res/figs/BoyanSlat.js
+++ common/ultimate/res/figs/BoyanSlat.js
@@ -4,9 +4,30 @@ module.exports = {
   color: `green`,
   age: 11,
   expansion: `figs`,
-  biscuits: ``,
-  dogmaBiscuit: ``,
-  karma: [],
-  karmaImpl: []
-}
+  biscuits: `shps`,
+  dogmaBiscuit: `s`,
+  karma: [
+    `If you would draw a card, first choose a value and return all cards of that value from all score piles. If you return exactly one card, achieve it, regardless of eligibility.`
+  ],
+  karmaImpl: [
+    {
+      trigger: 'draw',
+      kind: 'would-first',
+      matches: () => true,
+      func: (game, player) => {
+        const value = game.actions.chooseAge(player, game.util.ages())
+        const toReturn = game
+          .players
+          .all()
+          .flatMap(p => game.cards.byPlayer(p, 'score'))
+          .filter(card => card.getAge() === value)
 
+        const returned = game.actions.returnMany(player, toReturn)
+
+        if (returned.length === 1) {
+          game.actions.claimAchievement(player, returned[0])
+        }
+      }
+    }
+  ]
+}
