5c517a1af4d07173707b32bbc9a9b2dda7bb4e6c
dennis <dennis.horte@gmail.com>
2026-01-01 12:56:58 -0800

    Fire

--- common/ultimate/res/base/Fire.js
+++ common/ultimate/res/base/Fire.js
@@ -9,5 +9,38 @@ module.exports = {
     `I demand you reveal a card in your hand of color not on my board or of value greater than 0! If you don't, you lose!`,
     `Meld a card from your hand of color on your board. If you don't, you lose.`
   ],
-  dogmaImpl: [],
+  dogmaImpl: [
+    (game, player, { leader, self }) => {
+      const invalidColors = game.cards.tops(leader).map(card => card.color)
+      const revealChoices = game
+        .cards
+        .byPlayer(player, 'hand')
+        .filter(card => {
+          const notOnBoardCondition = !game.cards.top(leader, card.color)
+          const greaterThanZeroCondition = card.getAge() > 0
+          return notOnBoardCondition || greaterThanZeroCondition
+        })
+
+
+      const revealed = game.actions.chooseAndReveal(player, revealChoices)[0]
+
+      if (!revealed) {
+        game.aYouLose(player, self)
+      }
+    },
+    (game, player, { self }) => {
+      const validColors = game.cards.tops(player).map(card => card.color)
+      const meldChoices = game
+        .cards
+        .byPlayer(player, 'hand')
+        .filter(card => validColors.includes(card.color))
+
+      const melded = game.actions.chooseAndMeld(player, meldChoices)
+
+      if (!melded || (Array.isArray(melded) && melded.length === 0)) {
+        game.aYouLose(player, self)
+        return
+      }
+    },
+  ],
 }
