27363fbde502b8f94e9331a71e06ac5f6ab6de19
dennis <dennis.horte@gmail.com>
2026-01-01 15:20:08 -0800

    Hunting

--- common/ultimate/res/base/Hunting.js
+++ common/ultimate/res/base/Hunting.js
@@ -8,5 +8,14 @@ module.exports = {
   dogma: [
     `I demand you draw and reveal two {z}! Transfer the card of my choice to my board!`,
   ],
-  dogmaImpl: [],
+  dogmaImpl: [
+    (game, player, { leader, self }) => {
+      const drawnCards = [
+        game.actions.drawAndReveal(player, game.getEffectAge(self, 0)),
+        game.actions.drawAndReveal(player, game.getEffectAge(self, 0)),
+      ]
+
+      game.actions.chooseAndTransfer(leader, drawnCards, { toBoard: true, player: leader })
+    }
+  ],
 }
