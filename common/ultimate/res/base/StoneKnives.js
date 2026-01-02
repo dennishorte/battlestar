7e0b22f349fb72f5abcc5ab7ca44dfc6a72d6d3b
dennis <dennis.horte@gmail.com>
2026-01-01 17:03:23 -0800

    Stone Knives

--- common/ultimate/res/base/StoneKnives.js
+++ common/ultimate/res/base/StoneKnives.js
@@ -9,5 +9,22 @@ module.exports = {
     `I demand you draw, reveal, and return a {z}! Transfer your top card of the color of the revealed card to my board!`,
     `If Skinning is a top card on your board, draw a {1}.`
   ],
-  dogmaImpl: [],
+  dogmaImpl: [
+    (game, player, { leader, self }) => {
+      const card = game.actions.drawAndReveal(player, game.getEffectAge(self, 0))
+      game.actions.return(player, card)
+
+      const topCard = game.cards.top(player, card.color)
+      if (topCard) {
+        game.actions.transfer(player, topCard, game.zones.byPlayer(leader, card.color))
+      }
+    },
+    (game, player, { self }) => {
+      const topCards = game.cards.tops(player)
+      const skinning = topCards.find(card => card.name === 'Skinning')
+      if (skinning) {
+        game.actions.draw(player, { age: game.getEffectAge(self, 1) })
+      }
+    },
+  ],
 }
