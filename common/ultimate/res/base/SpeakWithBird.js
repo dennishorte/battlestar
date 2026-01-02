70eaf4ad5a3f93d2656f30cf3f663eb5391b8917
dennis <dennis.horte@gmail.com>
2026-01-01 16:47:31 -0800

    Speak with Bird

--- common/ultimate/res/base/SpeakWithBird.js
+++ common/ultimate/res/base/SpeakWithBird.js
@@ -8,5 +8,14 @@ module.exports = {
   dogma: [
     `If you have five top cards of value 0 on your board, you win.`
   ],
-  dogmaImpl: [],
+  dogmaImpl: [
+    (game, player, { self }) => {
+      const topCards = game.cards.tops(player)
+      const topValues = topCards.map(card => card.getAge())
+
+      if (topValues.length === 5 && topValues.every(value => value === 0)) {
+        game.youWin(player, self.name)
+      }
+    }
+  ],
 }
