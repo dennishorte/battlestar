ba4d8a08b46cdc5ae8d598f040369932b7d4b1ae
dennis <dennis.horte@gmail.com>
2026-01-24 17:54:30 -0800

    Add a test to Advancement to see if I can reproduce a crash in prod (it didn't reproduce, but I'll keep the test)

--- common/ultimate/res/figs/achievements/Advancement.test.js
+++ common/ultimate/res/figs/achievements/Advancement.test.js
@@ -2,24 +2,40 @@ Error.stackTraceLimit = 100
 
 const t = require('../../../testutil.js')
 
-test('Advancement', () => {
-  const game = t.fixtureDecrees()
-  game.testSetBreakpoint('before-first-player', (game) => {
-    t.setColor(game, 'dennis', 'red', ['Industrialization'])
-    t.setColor(game, 'dennis', 'purple', ['Monotheism'])
-    t.setDeckTop(game, 'base', 8, ['Flight'])
+describe('Advancement', () => {
+
+  test('basic', () => {
+    const game = t.fixtureDecrees()
+    game.testSetBreakpoint('before-first-player', (game) => {
+      t.setColor(game, 'dennis', 'red', ['Industrialization'])
+      t.setColor(game, 'dennis', 'purple', ['Monotheism'])
+      t.setDeckTop(game, 'base', 8, ['Flight'])
+    })
+
+    game.run()
+    t.choose(game, 'Decree.Advancement')
+
+    t.testIsSecondPlayer(game)
+    t.testBoard(game, {
+      dennis: {
+        red: ['Industrialization'],
+        purple: ['Monotheism'],
+        hand: ['Flight'], // Drew Flight from age 8 (highest top card age 6 + 2)
+        achievements: ['Advancement'], // Decree claimed as achievement
+      },
+    })
   })
 
-  game.run()
-  t.choose(game, 'Decree.Advancement')
+  test('get an age 12 card', () => {
+    const game = t.fixtureDecrees()
+    game.testSetBreakpoint('before-first-player', (game) => {
+      t.setColor(game, 'dennis', 'red', ['Astrogeology'])
+      t.setScore(game, 'dennis', ['Canning'])
+    })
 
-  t.testIsSecondPlayer(game)
-  t.testBoard(game, {
-    dennis: {
-      red: ['Industrialization'],
-      purple: ['Monotheism'],
-      hand: ['Flight'], // Drew Flight from age 8 (highest top card age 6 + 2)
-      achievements: ['Advancement'], // Decree claimed as achievement
-    },
+    game.run()
+    const request = t.choose(game, 'Decree.Advancement')
+
+    t.testGameOver(request, 'dennis', 'high draw')
   })
 })
