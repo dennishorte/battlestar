afa79f4c4989f4c3b283550d11e9200933d971b1
dennis <dennis.horte@gmail.com>
2026-02-07 10:08:37 -0800

    Add some scarring instruction to ScarRound component

--- app/src/modules/games/cube_draft/components/ScarRound.vue
+++ app/src/modules/games/cube_draft/components/ScarRound.vue
@@ -6,6 +6,10 @@
       {{ scar.text }}
     </BAlert>
 
+    <div>
+      To add a scar to a card, click the card you want to scar. A dialog will pop up that shows the two scars and the card editor. Make the appropriate edits to the card, and then click the button corresponding to the scar you used. You can add optional comments if you want.
+    </div>
+
   </div>
 </template>
 
