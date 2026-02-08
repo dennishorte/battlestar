5bb392d77eaa6657e29dd18f52b59132cc72912c
dennis <dennis.horte@gmail.com>
2026-02-08 10:06:53 -0800

    Fix a couple of issues in server-deploy.sh

--- scripts/server-deploy.sh
+++ scripts/server-deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Load nvm/node path for non-interactive shells
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Use the correct SSH key for GitHub
+export GIT_SSH_COMMAND="ssh -i ~/.ssh/github_personal -o IdentitiesOnly=yes"
+
 PROJ_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$PROJ_DIR"
 
