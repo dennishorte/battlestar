26148c167d7bb200f99c8ed8d294023bf3ad3eb5
dennis <dennis.horte@gmail.com>
2026-02-22 11:03:07 -0800

    Show map layout preview in Twilight lobby

--- app/src/modules/lobby/components/SettingsTwilight.vue
+++ app/src/modules/lobby/components/SettingsTwilight.vue
@@ -2,22 +2,38 @@
   <div class="settings-twilight">
     <div class="player-info">
       <span class="player-count">{{ playerCount }} player{{ playerCount !== 1 ? 's' : '' }}</span>
-      <span class="player-range">(3-6 players supported)</span>
+      <span class="player-range">(2-6 players, no 5-player layout)</span>
     </div>
     <div class="setting-note">
       Factions are assigned randomly at game start.
+    </div>
+    <div v-if="currentLayout" class="layout-preview">
+      <div class="layout-label">Board Layout</div>
+      <MapLayoutPreview :layout="currentLayout" />
+    </div>
+    <div v-else class="layout-warning">
+      No board layout available for {{ playerCount }} players.
     </div>
   </div>
 </template>
 
 <script>
+import { twilight } from 'battlestar-common'
+import MapLayoutPreview from './MapLayoutPreview.vue'
+
+const layouts = twilight.res.layouts
+
 export default {
   name: 'SettingsTwilight',
   inject: ['lobby', 'save'],
+  components: { MapLayoutPreview },
 
   computed: {
     playerCount() {
       return this.lobby.users.length
+    },
+    currentLayout() {
+      return layouts[this.playerCount] || null
     },
   },
 
@@ -32,8 +48,7 @@ export default {
 
   methods: {
     updateValid() {
-      const numPlayers = this.lobby.users.length
-      this.lobby.valid = numPlayers >= 3 && numPlayers <= 6
+      this.lobby.valid = this.playerCount in layouts
     },
   },
 
@@ -52,4 +67,7 @@ export default {
 .player-count { font-weight: 600; }
 .player-range { color: #666; font-size: .9em; }
 .setting-note { color: #888; font-size: .85em; }
+.layout-preview { margin-top: .75em; }
+.layout-label { font-size: .85em; color: #aaa; margin-bottom: .25em; }
+.layout-warning { margin-top: .75em; color: #c44; font-size: .9em; }
 </style>
